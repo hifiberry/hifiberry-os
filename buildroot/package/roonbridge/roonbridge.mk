a3dc385a8d533315ec1aa6d9dae836bd3f934edd
HiFiBerry <info@hifiberry.com>
2018-07-27 04:44:52 +0000

    Started working on Roonbridge (not ready yet)

---
+++ buildroot/package/roonbridge/roonbridge.mk
@@ -0,0 +1,33 @@
+################################################################################
+#
+# roonbridge
+#
+################################################################################
+
+ROONBRIDGE_SOURCE = RoonBridge_linuxarmv7hf.tar.bz2
+ROONBRIDGE_SITE = http://download.roonlabs.com/builds
+
+ROONBRIDGE_CHECK_BIN_ARCH_EXCLUSIONS = \
+	/opt/roon/RoonMono/lib/libmono-btls-shared.so \
+	/opt/roon/RoonMono/lib/libmono-btls-shared.so
+
+define ROONBRIDGE_BUILD_CMDS
+    echo "No build needed"
+endef
+
+define ROONBRIDGE_INSTALL_TARGET_CMDS
+    mkdir -p $(TARGET_DIR)/opt/roon
+    cp -rv $(@D)/* $(TARGET_DIR)/opt/roon
+endef
+
+define ROONBRIDGE_INSTALL_INIT_SYSV
+        $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/roonbridge/S99roonbridge \
+                $(TARGET_DIR)/etc/init.d/S99roonbridge
+endef
+
+define DSPTOOLKIT_INSTALL_INIT_SYSTEMD
+        $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/roonbridge/roonbridge.service \
+                $(TARGET_DIR)/lib/systemd/system/roonbridge.service
+endef
+
+$(eval $(generic-package))
