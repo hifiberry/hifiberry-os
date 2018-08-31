b263f1eceb491674d3da67cdea53f3e216917b77
matuschd <matuschd@storage.home>
2018-08-31 15:33:01 +0200

    DAC+ DSP testing working now

--- buildroot/package/hifiberry-test/hifiberry-test.mk
+++ buildroot/package/hifiberry-test/hifiberry-test.mk
@@ -7,14 +7,18 @@
 define HIFIBERRY_TEST_INSTALL_TARGET_CMDS
 	echo "HiFiBerry Test"
 	[ -d $(TARGET_DIR)/opt/hifiberry/contrib ] || mkdir -p $(TARGET_DIR)/opt/hifiberry/contrib
-#        $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/dsptoolkit/4way-iir.xml
-#           $(TARGET_DIR)/opt/hifiberry/contrib
+        $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-test/dspdac.xml \
+           $(TARGET_DIR)/opt/hifiberry/contrib
 endef
 
 define HIFIBERRY_TEST_INSTALL_INIT_SYSV_DSPDAC
 	echo "Installing DAC+ DSP test script"
 	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-test/S99testdacdsp \
 		$(TARGET_DIR)/etc/init.d/S99testdacdsp
+
+	echo "Adding drivers to config.txt"
+	echo "dtoverlay=hifiberry-dac" >> $(BINARIES_DIR)/rpi-firmware/config.txt
+	echo "dtparam=spi=on" >> $(BINARIES_DIR)/rpi-firmware/config.txt
 endef
 
 ifdef HIFIBERRY_TEST_DSPDAC
