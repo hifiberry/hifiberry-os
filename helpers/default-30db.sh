acdeffed4bb84243fafd11f693409300bc875686
HiFiBerry <info@hifiberry.com>
2018-10-11 13:51:14 +0200

    iSimple script to install a default profile with very low volume limit settings (-30db)

---
+++ helpers/default-30db.sh
@@ -0,0 +1,8 @@
+#i!/bin/bash
+dsptoolkit install-profile https://raw.githubusercontent.com/hifiberry/hifiberry-dsp/master/sample_files/xml/dacdsp-default.xml
+cat <<EOF >limit.txt
+volumeLimitRegister: -30db
+EOF
+dsptoolkit apply-settings ./limit.txt
+dsptoolkit store-settings ./limit.txt
+
