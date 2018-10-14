c36d126a1cd6a55e6767b0e11096392ec8aaf539
HiFiBerry <info@hifiberry.com>
2018-10-14 16:23:33 +0200

    Changed name of default configuration file

--- def-config.sh
+++ def-config.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd `dirname $0`/../buildroot
-make defconfig BR2_DEFCONFIG=../hifiberry-os/buildroot/configs/raspberypi3_defconfig
+make defconfig BR2_DEFCONFIG=../hifiberry-os/buildroot/configs/hifiberryos_config
 
 
--- save-defaultconfig.sh
+++ save-defaultconfig.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 cd `dirname $0`/../buildroot
-make BR2_EXTERNAL=../hifiberry-os/buildroot savedefconfig BR2_DEFCONFIG=../hifiberry-os/buildroot/configs/raspberypi3_defconfig
-mv ../hifiberry-os/buildroot/configs/raspberypi3_defconfig ../hifiberry-os/buildroot/configs/hifiberryos_defconfig
+make BR2_EXTERNAL=../hifiberry-os/buildroot savedefconfig BR2_DEFCONFIG=../hifiberry-os/buildroot/configs/hifiberryos_defconfig
