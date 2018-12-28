14c695e90d99b8d416bbae20c4f51d1a22c88b9a
HiFiBerry <info@hifiberry.com>
2018-12-28 13:06:30 +0100

    Added tests for DAC+ ADC

--- buildroot/package/hifiberry-test/hifiberry-test.mk
+++ buildroot/package/hifiberry-test/hifiberry-test.mk
@@ -9,6 +9,8 @@ define HIFIBERRY_TEST_INSTALL_TARGET_CMDS
 	[ -d $(TARGET_DIR)/opt/hifiberry/contrib ] || mkdir -p $(TARGET_DIR)/opt/hifiberry/contrib
         $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-test/dspdac.xml \
            $(TARGET_DIR)/opt/hifiberry/contrib
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-test/hifiberry-dacplusadc.dtbo \
+           $(TARGET_DIR)/opt/hifiberry/contrib
 endef
 
 define HIFIBERRY_TEST_INSTALL_INIT_SYSV_DSPDAC
@@ -21,9 +23,28 @@ define HIFIBERRY_TEST_INSTALL_INIT_SYSV_DSPDAC
 	echo "dtparam=spi=on" >> $(BINARIES_DIR)/rpi-firmware/config.txt
 endef
 
+define HIFIBERRY_TEST_INSTALL_INIT_SYSV_DACADC
+        echo "Installing DAC+ ADC test script"
+	mkdir -p $(TARGET_DIR)/boot/overlays/
+        $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-test/S99testdacadc \
+                $(TARGET_DIR)/etc/init.d/S99testdacadc
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-test/hifiberry-dacplusadc.dtbo \
+                $(TARGET_DIR)/boot/overlays/
+
+        echo "Adding drivers to config.txt"
+        echo "dtoverlay=hifiberry-dacplusadc" >> $(BINARIES_DIR)/rpi-firmware/config.txt
+        echo "dtparam=spi=on" >> $(BINARIES_DIR)/rpi-firmware/config.txt
+endef
+
 ifdef HIFIBERRY_TEST_DSPDAC
 HIFIBERRY_TEST_POST_INSTALL_TARGET_HOOKS += HIFIBERRY_TEST_INSTALL_INIT_SYSV_DSPDAC
 endif
+
+ifdef HIFIBERRY_TEST_DACADC
+HIFIBERRY_TEST_POST_INSTALL_TARGET_HOOKS += HIFIBERRY_TEST_INSTALL_INIT_SYSV_DACADC
+endif
+
+
 
 $(eval $(generic-package))
 
