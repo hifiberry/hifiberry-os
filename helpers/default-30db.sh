fdcaa4929961f7e0ec451adb1a606544983b1cd1
HiFiBerry <info@hifiberry.com>
2019-01-04 09:01:27 +0100

    Update default-30db.sh
    Bugfix: Removed extra character

--- helpers/default-30db.sh
+++ helpers/default-30db.sh
@@ -1,4 +1,4 @@
-#i!/bin/bash
+#!/bin/bash
 dsptoolkit install-profile https://raw.githubusercontent.com/hifiberry/hifiberry-dsp/master/sample_files/xml/dacdsp-default.xml
 cat <<EOF >limit.txt
 volumeLimitRegister: -30db
