02eb867d9a3f8ac023aed03622768fc2a8c37666
HiFiBerry <info@hifiberry.com>
2019-01-25 14:30:00 +0100

    Added Squeezelite

---
+++ buildroot/package/hifiberry-squeezelite/hifiberry-squeezelite.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# hifiberry-squeezelite
+#
+################################################################################
+
+define CONFIGTXT_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-squeezelite/S99squeezelite \
+                $(TARGET_DIR)/etc/init.d/S99squeezelite
+	mkdir -p $(TARGET_DIR)/var/squeezelite
+	echo "HiFiBerry" > $(TARGET_DIR)/var/squeezelite/squeezelite.name
+endef
+
+$(eval $(generic-package))
