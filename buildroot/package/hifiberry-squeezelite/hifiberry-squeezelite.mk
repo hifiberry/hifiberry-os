6f48f8f97677453b9921ce1a6fe11b594f300d75
HiFiBerry <info@hifiberry.com>
2019-01-31 16:16:07 +0100

    Bugfix: package name

--- buildroot/package/hifiberry-squeezelite/hifiberry-squeezelite.mk
+++ buildroot/package/hifiberry-squeezelite/hifiberry-squeezelite.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-define CONFIGTXT_INSTALL_TARGET_CMDS
+define HIFIBERRY_SQUEEZELITE_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-squeezelite/S99squeezelite \
                 $(TARGET_DIR)/etc/init.d/S99squeezelite
 	mkdir -p $(TARGET_DIR)/var/squeezelite
