da3f1c806e5b80685c35f9e9fe7b119cd9d46469
HiFiBerry <info@hifiberry.com>
2019-06-04 12:04:30 +0200

    Added DSP DAC/ADC addon tests

--- buildroot/package/hifiberry-test/hifiberry-test.mk
+++ buildroot/package/hifiberry-test/hifiberry-test.mk
@@ -48,17 +48,38 @@ define HIFIBERRY_TEST_INSTALL_INIT_SYSV_DACADC
         echo "dtparam=spi=on" >> $(BINARIES_DIR)/rpi-firmware/config.txt
 endef
 
+define HIFIBERRY_TEST_INSTALL_INIT_SYSV_DACADC
+        echo "Installing USB2I2S test script"
+        mkdir -p $(TARGET_DIR)/opt/hifiberry/bin
+        $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-test/S99testusb \
+                $(TARGET_DIR)/etc/init.d/S99testusb
+        $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-test/spi_flash \
+                $(TARGET_DIR)/opt/hifiberry/bin/
+	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-test/hbfw_usb2i2s_100.bin \
+		$(TARGET_DIR)/opt/hifiberry/bin/
+
+        echo "Adding drivers to config.txt"
+        echo "dtparam=spi=on" >> $(BINARIES_DIR)/rpi-firmware/config.txt
+endef
+
+
 ifdef HIFIBERRY_TEST_DSPDAC
 HIFIBERRY_TEST_POST_INSTALL_TARGET_HOOKS += HIFIBERRY_TEST_INSTALL_INIT_SYSV_DSPDAC
 endif
 
 ifdef HIFIBERRY_TEST_DSPDACADC
-HIFIBERRY_TEST_POST_INSTALL_TARGET_HOOKS += HIFIBERRY_TEST_INSTALL_INIT_SYSV_DSPDAC
+HIFIBERRY_TEST_POST_INSTALL_TARGET_HOOKS += HIFIBERRY_TEST_INSTALL_INIT_SYSV_DSPDACADC
 endif
 
 ifdef HIFIBERRY_TEST_DACADC
 HIFIBERRY_TEST_POST_INSTALL_TARGET_HOOKS += HIFIBERRY_TEST_INSTALL_INIT_SYSV_DACADC
 endif
+
+ifdef HIFIBERRY_TEST_USB
+HIFIBERRY_TEST_POST_INSTALL_TARGET_HOOKS += HIFIBERRY_TEST_INSTALL_INIT_SYSV_USB
+endif
+
+
 
 
 
