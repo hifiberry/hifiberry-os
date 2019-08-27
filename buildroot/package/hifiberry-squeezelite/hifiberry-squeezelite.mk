687aa0b3226c7b3aab48a6facc0d950ceefc2bbe
HiFiBerry <info@hifiberry.com>
2019-08-27 10:19:47 +0200

    Enabled systemd

--- buildroot/package/hifiberry-squeezelite/hifiberry-squeezelite.mk
+++ buildroot/package/hifiberry-squeezelite/hifiberry-squeezelite.mk
@@ -11,4 +11,16 @@ define HIFIBERRY_SQUEEZELITE_INSTALL_TARGET_CMDS
 	echo "HiFiBerry" > $(TARGET_DIR)/var/squeezelite/squeezelite.name
 endef
 
+define HIFIBERRY_SQUEEZELITE_INSTALL_INIT_SYSV
+        $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-squeezelite/S99squeezelite \
+                $(TARGET_DIR)/etc/init.d/S99squeezelite
+endef
+
+define HIFIBERRY_SQUEEZELITE_INSTALL_INIT_SYSTEMD
+        $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-squeezelite/squeezelite.service \
+                $(TARGET_DIR)/usr/lib/systemd/system/squeezelite.service
+        ln -fs ../../../../usr/lib/systemd/system/squeezelite.service \
+                $(TARGET_DIR)/usr/lib/systemd/system/multi-user.target.wants/squeezelite.service
+endef
+
 $(eval $(generic-package))
