dfc10a1b10fbc8502ef6d39285a79077c1bd46f0
HiFiBerry <info@hifiberry.com>
2019-08-27 10:22:17 +0200

    Enabled systemd for shairport

---
+++ buildroot/package/hifiberry-shairport/hifiberry-shairport.mk
@@ -0,0 +1,14 @@
+################################################################################
+#
+# hifiberry-shairport
+#
+################################################################################
+
+define HIFIBERRY_SHAIRPORT_INSTALL_INIT_SYSTEMD
+        $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-shairport/shairport-sync.service \
+                $(TARGET_DIR)/usr/lib/systemd/system/shairport-sync.service
+        ln -fs ../../../../usr/lib/systemd/system/shairport-sync.service \
+                $(TARGET_DIR)/usr/lib/systemd/system/multi-user.target.wants/shairport-sync.service
+endef
+
+$(eval $(generic-package))
