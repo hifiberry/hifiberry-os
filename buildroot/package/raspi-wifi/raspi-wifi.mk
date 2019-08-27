0ef6bcdb7ecb63a2fe3265a4f506e769dabc389a
HiFiBerry <info@hifiberry.com>
2019-08-27 10:21:20 +0200

    Enabled systemd

--- buildroot/package/raspi-wifi/raspi-wifi.mk
+++ buildroot/package/raspi-wifi/raspi-wifi.mk
@@ -14,7 +14,9 @@ endef
 
 define RASPI_WIFI_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/raspi-wifi/interfaces \
-           $(TARGET_DIR)/etc/network/interfaces
+           $(TARGET_DIR)/etc/network/interfaces.bak
+    $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/raspi-wifi/copy-config \
+           $(TARGET_DIR)/opt/hifiberry/bin/copy-config
 endef
 
 define RASPI_WIFI_INSTALL_INIT_SYSV
@@ -22,7 +24,22 @@ define RASPI_WIFI_INSTALL_INIT_SYSV
            $(TARGET_DIR)/etc/init.d/
 endef
 
-define DSPTOOLKIT_INSTALL_INIT_SYSTEMD
+define RASPI_WIFI_INSTALL_INIT_SYSTEMD
+    $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/raspi-wifi/copy-config.service \
+           $(TARGET_DIR)/rsr/lib/systemd/system/copy-config.service
+    ln -fs ../../../usr/lib/systemd/system/copy-config.service \
+           $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/copy-config.service
+    $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/raspi-wifi/wireless.network \
+           $(TARGET_DIR)/etc/systemd/network/wireless.network
+    $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/raspi-wifi/wpa_supplicant@wlan0.service \
+           $(TARGET_DIR)/usr/lib/systemd/system/wpa_supplicant@wlan0.service
+    ln -fs ../../../../usr/lib/systemd/system/wpa_supplicant@wlan0.service \
+           $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/wpa_supplicant@wlan0.service
+#    ln -fs ../../../usr/lib/systemd/system/wpa_supplicant.service \
+#           $(TARGET_DIR)/etc/systemd/system/dbus-fi.w1.wpa_supplicant1.service
+#    ln -fs ../../../../usr/lib/systemd/system/wpa_supplicant.service \
+#           $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/wpa_supplicant.service
+
 endef
 
 $(eval $(generic-package))
