2b1281b1a7a96ddea5201817879e539542a87d59
HiFiBerry <info@hifiberry.com>
2019-08-27 10:17:53 +0200

    Enabled systemd startup

--- buildroot/package/dsptoolkit/dsptoolkit.mk
+++ buildroot/package/dsptoolkit/dsptoolkit.mk
@@ -12,6 +12,8 @@ DSPTOOLKIT_LICENSE = MIT
 DSPTOOLKIT_LICENSE_FILES = LICENSE.md
 
 define DSPTOOLKIT_POST_INSTALL_STAGING_CMD
+	echo "DSPTk 1"
+        sleep 10
 	[ -d $(TARGET_DIR)/opt/hifiberry/contrib ] || mkdir $(TARGET_DIR)/opt/hifiberry/contrib
 	$(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/dsptoolkit/4way-iir.xml
            $(TARGET_DIR)/opt/hifiberry/contrib
@@ -24,17 +26,15 @@ endef
 
 DSPTOOLKIT_POST_INSTALL_STAGING_HOOKS += DSPTOOLKIT_POST_INSTALL_STAGING_CMD
 
-define DSPTOOLKIT_INSTALL_INIT_SYSV
-	pwd
+define DSPTOOLKIT_INSTALL_INIT
 	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/dsptoolkit/S90sigmatcp \
 		$(TARGET_DIR)/etc/init.d/S90sigmatcp
-endef
-
-define DSPTOOLKIT_INSTALL_INIT_SYSTEMD
         $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/dsptoolkit/sigmatcp.service \
-                $(TARGET_DIR)/lib/systemd/system/sigmatcp.service
+                $(TARGET_DIR)/usr/lib/systemd/system/sigmatcp.service
+        ln -fs ../../../../../usr/lib/systemd/system/sigmatcp.service \
+                $(TARGET_DIR)/usr/lib/systemd/system/multi-user.target.wants/sigmatcp.service
 endef
 
-DSPTOOLKIT_POST_INSTALL_STAGING_HOOKS += DSPTOOLKIT_INSTALL_INIT_SYSV
+DSPTOOLKIT_POST_INSTALL_TARGET_HOOKS += DSPTOOLKIT_INSTALL_INIT
 
 $(eval $(python-package))
