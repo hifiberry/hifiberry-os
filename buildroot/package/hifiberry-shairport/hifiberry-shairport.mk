b324284078bdb287a941d2d8e42daea517678cb9
HiFiBerry <info@hifiberry.com>
2019-09-06 11:08:18 +0200

    Enabled MPRIS

--- buildroot/package/hifiberry-shairport/hifiberry-shairport.mk
+++ buildroot/package/hifiberry-shairport/hifiberry-shairport.mk
@@ -4,6 +4,60 @@
 #
 ################################################################################
 
+HIFIBERRY_SHAIRPORT_VERSION = 3.2.2
+HIFIBERRY_SHAIRPORT_SITE = $(call github,mikebrady,shairport-sync,$(HIFIBERRY_SHAIRPORT_VERSION))
+
+HIFIBERRY_SHAIRPORT_LICENSE = MIT, BSD-3-Clause
+HIFIBERRY_SHAIRPORT_LICENSE_FILES = LICENSES
+HIFIBERRY_SHAIRPORT_DEPENDENCIES = alsa-lib libconfig libdaemon popt host-pkgconf
+
+# git clone, no configure
+HIFIBERRY_SHAIRPORT_AUTORECONF = YES
+
+HIFIBERRY_SHAIRPORT_CONF_OPTS = --with-alsa \
+        --with-metadata \
+        --with-pipe \
+        --with-stdout \
+        --with-avahi \
+        --with-mpris-interface \
+        --with-mpris-test-client 
+
+HIFIBERRY_SHAIRPORT_CONF_ENV += LIBS="$(HIFIBERRY_SHAIRPORT_CONF_LIBS)"
+
+# OpenSSL or mbedTLS
+ifeq ($(BR2_PACKAGE_OPENSSL),y)
+HIFIBERRY_SHAIRPORT_DEPENDENCIES += openssl
+HIFIBERRY_SHAIRPORT_CONF_OPTS += --with-ssl=openssl
+else
+HIFIBERRY_SHAIRPORT_DEPENDENCIES += mbedtls
+HIFIBERRY_SHAIRPORT_CONF_OPTS += --with-ssl=mbedtls
+HIFIBERRY_SHAIRPORT_CONF_LIBS += -lmbedx509 -lmbedcrypto
+ifeq ($(BR2_PACKAGE_MBEDTLS_COMPRESSION),y)
+HIFIBERRY_SHAIRPORT_CONF_LIBS += -lz
+endif
+endif
+
+ifeq ($(BR2_PACKAGE_HIFIBERRY_SHAIRPORT_LIBSOXR),y)
+HIFIBERRY_SHAIRPORT_DEPENDENCIES += libsoxr
+HIFIBERRY_SHAIRPORT_CONF_OPTS += --with-soxr
+endif
+
+define HIFIBERRY_SHAIRPORT_INSTALL_TARGET_CMDS
+        $(INSTALL) -D -m 0755 $(@D)/shairport-sync \
+                $(TARGET_DIR)/usr/bin/shairport-sync
+        $(INSTALL) -D -m 0755 $(@D)/shairport-sync-mpris-test-client \
+                $(TARGET_DIR)/usr/bin/shairport-sync-mpris-test-client
+        $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-shairport/shairport-sync.conf \
+                $(TARGET_DIR)/etc/shairport-sync.conf
+        $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-shairport/dbus.conf \
+                $(TARGET_DIR)/etc/dbus-1/system.d/spotify.conf
+endef
+
+define HIFIBERRY_SHAIRPORT_INSTALL_INIT_SYSV
+        $(INSTALL) -D -m 0755 package/shairport-sync/S99shairport-sync \
+                $(TARGET_DIR)/etc/init.d/S99shairport-sync
+endef
+
 define HIFIBERRY_SHAIRPORT_INSTALL_INIT_SYSTEMD
         $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-shairport/shairport-sync.service \
                 $(TARGET_DIR)/usr/lib/systemd/system/shairport-sync.service
@@ -11,4 +65,4 @@ define HIFIBERRY_SHAIRPORT_INSTALL_INIT_SYSTEMD
                 $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/shairport-sync.service
 endef
 
-$(eval $(generic-package))
+$(eval $(autotools-package))
