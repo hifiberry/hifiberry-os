f87bd40436f44daab983d08cc9f2851e525e4bf0
HiFiBerry <info@hifiberry.com>
2019-09-06 11:07:05 +0200

    Moved some code to another script

--- build-all.sh
+++ build-all.sh
@@ -9,10 +9,6 @@ for i in 0w 3 4; do
   ./clean.sh
   ./build-config.sh $i
   ./compile.sh
-  mv ../buildroot/output/images/sdcard.img images/hifiberryos-$TS-pi$i.img
-  pushd images
-  zip hifiberryos-pi$i.zip hifiberryos-$TS-pi$i.img
-  popd
-  sleep 600
+  ./create-image $i
 done
 
---
+++ create-image.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+PLATFORM=$1
+if [ "$PLATFORM" == "" ]; then
+ echo Call with $0 platform where platform is 0w, 3 or 4
+ exit 1
+fi
+TS=`date +%Y%m%d`
+cd `dirname $0`
+MYDIR=`pwd`
+echo $MYDIR
+cp ../buildroot/output/images/sdcard.img images/hifiberryos-$TS-pi$PLATFORM.img
+pushd images
+zip hifiberryos-pi$i.zip hifiberryos-$TS-pi$PLATFORM.img
+popd
+TMPDIR=/tmp/$$
+mkdir $TMPDIR
+cp ../buildroot/output/images/zImage ../buildroot/output/images/rootfs.ext2 $TMPDIR
+pushd $TMPDIR
+pwd
+ls
+tar cvfz $MYDIR/images/updater-$TS-pi$i.tar.gz zImage rootfs.ext2
+popd
+rm -rf $TMPDIR
