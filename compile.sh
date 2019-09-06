5af30d55147a60a0fa28b7dc022804fd8cc40f7f
HiFiBerry <info@hifiberry.com>
2019-09-06 11:09:27 +0200

    Removed unused code

--- compile.sh
+++ compile.sh
@@ -7,18 +7,4 @@ if [ "$1" != "" ]; then
  rm -rf output/build/$1*
 fi
 
-if [ "$1" == "1" ]; then
- sed -i "s/BR2_LINUX_KERNEL_DEFCONFIG.*/BR2_LINUX_KERNEL_DEFCONFIG=\"bcmrpi\"/" .config
- make linux-dirclean
- make linux-rebuild
-elif [ "$1" == "3" ]; then
- sed -i "s/BR2_LINUX_KERNEL_DEFCONFIG.*/BR2_LINUX_KERNEL_DEFCONFIG=\"bcm2709\"/" .config
- make linux-dirclean
- make linux-rebuild
-elif [ "$1" == "4" ]; then
- sed -i "s/BR2_LINUX_KERNEL_DEFCONFIG.*/BR2_LINUX_KERNEL_DEFCONFIG=\"bcm2711\"/" .config
- make linux-dirclean
- make linux-rebuild
-fi
-
-make -j 6 BR2_INSTRUMENTATION_SCRIPTS="$MYDIR/log.sh"
+make
