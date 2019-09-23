88952ef33e41402ba6ac06eff644fb3f91710f94
HiFiBerry <info@hifiberry.com>
2019-09-23 16:57:02 +0200

    Added Audio controller 2

---
+++ buildroot/package/audiocontrol2/audiocontrol2.mk
@@ -0,0 +1,27 @@
+################################################################################
+#
+# audiocontrol2
+#
+################################################################################
+
+AUDIOCONTROL2_VERSION = 0c98af8b41951b54d088b59764a4fb93fefddfd6
+AUDIOCONTROL2_SITE = $(call github,hifiberry,audiocontrol2,$(AUDIOCONTROL2_VERSION))
+
+define AUDIOCONTROL2_BUILD_CMDS
+endef
+
+define AUDIOCONTROL2_INSTALL_TARGET_CMDS
+        mkdir -p $(TARGET_DIR)/opt/audiocontrol2
+        cp -rv $(@D)/* $(TARGET_DIR)/opt/audiocontrol2
+	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/audiocontrol2/audiocontrol2.conf \
+		$(TARGET_DIR)/etc
+endef
+
+define AUDIOCONTROL2_INSTALL_INIT_SYSTEMD
+    $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/audiocontrol2/audiocontrol2.service \
+           $(TARGET_DIR)/usr/lib/systemd/system/audiocontrol2.service
+    ln -fs ../../../usr/lib/systemd/system/audiocontrol2.service \
+           $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/audiocontrol2.service
+endef
+
+$(eval $(generic-package))
