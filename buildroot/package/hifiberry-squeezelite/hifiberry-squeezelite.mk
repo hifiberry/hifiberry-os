59fa02a4fa9a4d460bcaac8cf589197f5e2407e9
HiFiBerry <info@hifiberry.com>
2019-09-23 16:46:35 +0200

    Squezzelite now uses the correct mixer control

--- buildroot/package/hifiberry-squeezelite/hifiberry-squeezelite.mk
+++ buildroot/package/hifiberry-squeezelite/hifiberry-squeezelite.mk
@@ -5,10 +5,10 @@
 ################################################################################
 
 define HIFIBERRY_SQUEEZELITE_INSTALL_TARGET_CMDS
-	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-squeezelite/S99squeezelite \
-                $(TARGET_DIR)/etc/init.d/S99squeezelite
 	mkdir -p $(TARGET_DIR)/var/squeezelite
 	echo "HiFiBerry" > $(TARGET_DIR)/var/squeezelite/squeezelite.name
+        $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-squeezelite/volume-control-name \
+                $(TARGET_DIR)/opt/hifiberry/bin/volume-control-name
 endef
 
 define HIFIBERRY_SQUEEZELITE_INSTALL_INIT_SYSV
