07a809ba73e03408adc036651447f242dd753dd7
HiFiBerry <info@hifiberry.com>
2019-09-23 16:53:25 +0200

    Does not automatically reboot now

--- buildroot/package/hifiberry-updater/hifiberry-updater.mk
+++ buildroot/package/hifiberry-updater/hifiberry-updater.mk
@@ -9,6 +9,8 @@ define HIFIBERRY_UPDATER_INSTALL_TARGET_CMDS
                 $(TARGET_DIR)/opt/hifiberry/bin
         $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-updater/partitions \
                 $(TARGET_DIR)/opt/hifiberry/bin
+        $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/VERSION \
+                $(TARGET_DIR)/etc/hifiberry.version
 endef
 
 define HIFIBERRY_UPDATER_INSTALL_INIT_SYSTEMD
