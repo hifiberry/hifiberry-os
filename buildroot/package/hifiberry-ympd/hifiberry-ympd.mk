d44aee5f549cdd229ba2dc8da80516599b8221ea
HiFiBerry <info@hifiberry.com>
2019-09-23 16:58:40 +0200

    Added yMPD web interface

---
+++ buildroot/package/hifiberry-ympd/hifiberry-ympd.mk
@@ -0,0 +1,19 @@
+################################################################################
+#
+# hifiberry-ympd
+#
+################################################################################
+
+#define HIFIBERRY_YMPD_INSTALL_TARGET_CMDS
+#	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/ympd/ympd-bin \
+#                $(TARGET_DIR)/usr/bin/ympd
+#endef
+
+define HIFIBERRY_YMPD_INSTALL_INIT_SYSTEMD
+        $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-ympd/ympd.service \
+                $(TARGET_DIR)/usr/lib/systemd/system/ympd.service
+        ln -fs ../../../usr/lib/systemd/system/ympd.service \
+                $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/ympd.service
+endef
+
+$(eval $(generic-package))
