5ccacd2fcc97a3388a4de79c42c073b8d144a978
HiFiBerry <info@hifiberry.com>
2019-09-24 16:25:24 +0200

    Corrected systemd symlink

--- buildroot/package/hifiberry-mpd/hifiberry-mpd.mk
+++ buildroot/package/hifiberry-mpd/hifiberry-mpd.mk
@@ -19,7 +19,7 @@ endef
 define HIFIBERRY_MPD_INSTALL_INIT_SYSTEMD
         $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-mpd/mpd.service \
                 $(TARGET_DIR)/usr/lib/systemd/system/mpd.service
-        ln -fs ../../../usr/lib/systemd/system/shairport-sync.service \
+        ln -fs ../../../../usr/lib/systemd/system/shairport-sync.service \
                 $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/mpd.service
 endef
 
