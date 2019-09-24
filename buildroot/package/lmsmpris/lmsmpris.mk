5c0cabd3b10892e0d2d2f54314c2ae1a83488388
HiFiBerry <info@hifiberry.com>
2019-09-24 16:29:15 +0200

    Corrected systemd symlinks

--- buildroot/package/lmsmpris/lmsmpris.mk
+++ buildroot/package/lmsmpris/lmsmpris.mk
@@ -18,7 +18,7 @@ endef
 define LMSMPRIS_INSTALL_INIT_SYSTEMD
     $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/lmsmpris/lmsmpris.service \
            $(TARGET_DIR)/usr/lib/systemd/system/lmsmpris.service
-    ln -fs ../../../usr/lib/systemd/system/lmsmpris.service \
+    ln -fs ../../../../usr/lib/systemd/system/lmsmpris.service \
            $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/lmsmpris.service
 endef
 
