8fcd8d3f1f14615b25957fcc898147dea494f521
HiFiBerry <info@hifiberry.com>
2019-09-24 16:50:27 +0200

    Updated to support the Pi2

--- build-all.sh
+++ build-all.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd `dirname $0`
 TS=`date +%Y%m%d`
-for i in 0w 2 3 4; do
+echo "VERSION=$TS" > images/listing
+for i in 0w 2 4 3; do
   clear
   echo Buildung for Raspberry Pi $i
   echo ============================
@@ -9,5 +10,6 @@ for i in 0w 2 3 4; do
   ./build-config.sh $i
   ./compile.sh
   ./create-image.sh $i
+  echo $TS > images/VERSION
+  echo "UPDATE${i}=updater-$TS-pi${o}.tar.gz" >> images/listing
 done
-
