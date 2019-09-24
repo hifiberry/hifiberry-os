84027e76ebd07a312ccc10c2b2a546f8a92863d6
HiFiBerry <info@hifiberry.com>
2019-09-24 16:28:07 +0200

    Added update script Fixed a bug in extract-update (configs were not restored correctly)

--- buildroot/package/hifiberry-updater/hifiberry-updater.mk
+++ buildroot/package/hifiberry-updater/hifiberry-updater.mk
@@ -5,12 +5,17 @@
 ################################################################################
 
 define HIFIBERRY_UPDATER_INSTALL_TARGET_CMDS
+        $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-updater/update \
+                $(TARGET_DIR)/opt/hifiberry/bin
 	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-updater/extract-update \
                 $(TARGET_DIR)/opt/hifiberry/bin
         $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-updater/partitions \
                 $(TARGET_DIR)/opt/hifiberry/bin
-        $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/VERSION \
+        $(INSTALL) -D -m 0444 $(BR2_EXTERNAL_HIFIBERRY_PATH)/VERSION \
                 $(TARGET_DIR)/etc/hifiberry.version
+        $(INSTALL) -D -m 0444 $(BR2_EXTERNAL_HIFIBERRY_PATH)/PIVERSION \
+                $(TARGET_DIR)/etc/raspberrypi.version
+
 endef
 
 define HIFIBERRY_UPDATER_INSTALL_INIT_SYSTEMD
