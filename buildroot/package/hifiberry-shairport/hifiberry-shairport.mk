ccb2325c18eba9ae008558e7249c8262b35273cc
HiFiBerry <info@hifiberry.com>
2019-09-24 16:25:48 +0200

    Corrected systemd symlink

--- buildroot/package/hifiberry-shairport/hifiberry-shairport.mk
+++ buildroot/package/hifiberry-shairport/hifiberry-shairport.mk
@@ -61,7 +61,7 @@ endef
 define HIFIBERRY_SHAIRPORT_INSTALL_INIT_SYSTEMD
         $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-shairport/shairport-sync.service \
                 $(TARGET_DIR)/usr/lib/systemd/system/shairport-sync.service
-        ln -fs ../../../usr/lib/systemd/system/shairport-sync.service \
+        ln -fs ../../../../usr/lib/systemd/system/shairport-sync.service \
                 $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/shairport-sync.service
 endef
 
