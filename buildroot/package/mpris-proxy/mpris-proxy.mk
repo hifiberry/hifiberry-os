71acb19cf51162ef8fc735ba719aee69ae80b798
HiFiBerry <info@hifiberry.com>
2019-09-24 16:29:59 +0200

    Corrected systemd symlinks

--- buildroot/package/mpris-proxy/mpris-proxy.mk
+++ buildroot/package/mpris-proxy/mpris-proxy.mk
@@ -10,10 +10,10 @@ define MPRIS_PROXY_INSTALL_TARGET_CMDS
            $(TARGET_DIR)/etc/dbus-1/system.d/mpris-proxy.conf
 endef
 
-define SPOTIFYD_INSTALL_INIT_SYSTEMD
+define MPRIS_PROXY_INSTALL_INIT_SYSTEMD
     $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/mpris-proxy/mpris-proxy.service \
            $(TARGET_DIR)/usr/lib/systemd/system/mpris-proxy.service
-    ln -fs ../../../usr/lib/systemd/system/mpris-proxy.service \
+    ln -fs ../../../../usr/lib/systemd/system/mpris-proxy.service \
            $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/mpris-proxy.service
 endef
 
