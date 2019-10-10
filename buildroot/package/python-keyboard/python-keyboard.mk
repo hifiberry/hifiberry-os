a635ba06c816261de19e1220381016a17002a22a
HiFiBerry <info@hifiberry.com>
2019-10-10 19:37:03 +0200

    Bump to latest version

--- buildroot/package/python-keyboard/python-keyboard.mk
+++ buildroot/package/python-keyboard/python-keyboard.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_KEYBOARD_VERSION = 0.13.2
+PYTHON_KEYBOARD_VERSION = 0.13.4
 PYTHON_KEYBOARD_SITE = $(call github,boppreh,keyboard,master)
 PYTHON_KEYBOARD_SETUP_TYPE = setuptools
 PYTHON_KEYBOARD_LICENSE = MIT
