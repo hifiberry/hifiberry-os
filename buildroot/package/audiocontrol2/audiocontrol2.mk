31c180cdbb4bb05e9c0df62aa5265631125a564b
HiFiBerry <info@hifiberry.com>
2019-10-10 19:34:46 +0200

    Added more features: - keyboard / remote control support - HTTP metadata push

--- buildroot/package/audiocontrol2/audiocontrol2.mk
+++ buildroot/package/audiocontrol2/audiocontrol2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-AUDIOCONTROL2_VERSION = 44b1c5d6ca9e9bcb16da50e6a893c5fb8d93da3e
+AUDIOCONTROL2_VERSION = 83c518b7a6afc08098f9ec3fb62e2fe497dd3d68
 AUDIOCONTROL2_SITE = $(call github,hifiberry,audiocontrol2,$(AUDIOCONTROL2_VERSION))
 
 define AUDIOCONTROL2_BUILD_CMDS
