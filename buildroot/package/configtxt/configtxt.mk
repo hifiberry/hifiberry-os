71bee0abbed7c362e0e426632c0c20b4eebb79c9
HiFiBerry <info@hifiberry.com>
2019-10-18 17:52:44 +0200

    Add changes to cmdline.txt

--- buildroot/package/configtxt/configtxt.mk
+++ buildroot/package/configtxt/configtxt.mk
@@ -10,5 +10,14 @@ define CONFIGTXT_INSTALL_TARGET_CMDS
 	echo "dtparam=spi=on" >> $(BINARIES_DIR)/rpi-firmware/config.txt
 endef
 
+define CONFIGTXT_QUIET_INSTALL_TARGET_CMDS
+        $(INSTALL) -D -m 644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/configtxt/cmdline.quiet \
+                $(BINARIES_DIR)/rpi-firmware/cmdline.txt
+endef
+
+ifeq ($(BR2_PACKAGE_CONFIGTXT_QUIET),y)
+CONFIGTXT_POST_INSTALL_TARGET_HOOKS += CONFIGTXT_QUIET_INSTALL_TARGET_CMDS
+endif
+
 $(eval $(generic-package))
 
