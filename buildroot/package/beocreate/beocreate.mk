3fa2ce23869a83f0be08d9fa05da02957464c11a
HiFiBerry <info@hifiberry.com>
2019-10-20 17:18:02 +0000

    Added local systemd service file to match hifiberry target

--- buildroot/package/beocreate/beocreate.mk
+++ buildroot/package/beocreate/beocreate.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BEOCREATE_VERSION = c771c7a03217d15a171ea89d7b3f1847d85c75e2
+BEOCREATE_VERSION = 006e654769ed5fa3f212d5ff0f88f5a3449ece39
 BEOCREATE_SITE = $(call github,bang-olufsen,create,$(BEOCREATE_VERSION))
 
 define BEOCREATE_BUILD_CMDS
@@ -20,8 +20,9 @@ define BEOCREATE_INSTALL_TARGET_CMDS
 endef
 
 define BEOCREATE_INSTALL_INIT_SYSTEMD
-	cp -rv $(@D)/beocreate2.service $(TARGET_DIR)/usr/lib/systemd/system/beocreate2.service
-    ln -fs ../../../../usr/lib/systemd/system/beocreate2.service \
+        $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/beocreate/beocreate2.service \
+                $(TARGET_DIR)/lib/systemd/system/beocreate2.service
+	ln -fs ../../../../usr/lib/systemd/system/beocreate2.service \
            $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/beocreate2.service
 endef
 
