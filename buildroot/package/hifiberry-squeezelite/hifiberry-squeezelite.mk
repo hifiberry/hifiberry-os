de7581a9e5b3ab08d71c67a27e41c8ce8a576cbb
HiFiBerry <info@hifiberry.com>
2019-10-25 14:21:23 +0200

    Disable by default, change command line (to fix a problem with sound not working after some time)

--- buildroot/package/hifiberry-squeezelite/hifiberry-squeezelite.mk
+++ buildroot/package/hifiberry-squeezelite/hifiberry-squeezelite.mk
@@ -19,8 +19,8 @@ endef
 define HIFIBERRY_SQUEEZELITE_INSTALL_INIT_SYSTEMD
         $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-squeezelite/squeezelite.service \
                 $(TARGET_DIR)/usr/lib/systemd/system/squeezelite.service
-        ln -fs ../../../../usr/lib/systemd/system/squeezelite.service \
-                $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/squeezelite.service
+#        ln -fs ../../../../usr/lib/systemd/system/squeezelite.service \
+#                $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/squeezelite.service
 endef
 
 $(eval $(generic-package))
