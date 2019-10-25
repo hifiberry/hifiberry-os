4518145e96e0f066a55a4b5cc70456bd3f93dbd4
HiFiBerry <info@hifiberry.com>
2019-10-25 09:12:47 +0200

    Changed build system to support parallel builds for multiple platforms

--- save-config.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd `dirname $0`/../buildroot
-NAME=$1
-if [ "$NAME" == "" ]; then
- 	NAME=hifiberryos_defconfig
-fi
-make BR2_EXTERNAL=../hifiberry-os/buildroot savedefconfig BR2_DEFCONFIG=../hifiberry-os/buildroot/configs/$NAME
