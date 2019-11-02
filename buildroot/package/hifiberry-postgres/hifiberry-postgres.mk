ecdbede80939b1e8cbd4365e71820c2a7e0ef90d
HiFiBerry <info@hifiberry.com>
2019-11-02 12:07:07 +0100

    Improved startup stability Added script to show scrobbles

--- buildroot/package/hifiberry-postgres/hifiberry-postgres.mk
+++ buildroot/package/hifiberry-postgres/hifiberry-postgres.mk
@@ -7,6 +7,8 @@
 define HIFIBERRY_POSTGRES_INSTALL_TARGET_CMDS
         $(INSTALL) -D -m 0555 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-postgres/create-hbdb \
                 $(TARGET_DIR)/opt/hifiberry/helpers/create-hbdb
+        $(INSTALL) -D -m 0555 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-postgres/show-scrobbles \
+                $(TARGET_DIR)/opt/hifiberry/bin/show-scrobbles
 endef
 
 define HIFIBERRY_POSTGRES_INSTALL_INIT_SYSTEMD
