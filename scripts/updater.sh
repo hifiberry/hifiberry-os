b15ec2369117e623fdc237a3abb200ffb100bac7
HiFiBerry <info@hifiberry.com>
2019-11-03 15:20:01 +0100

    Add postgres configuration

--- scripts/updater.sh
+++ scripts/updater.sh
@@ -17,3 +17,17 @@ if [ "$V" -lt 20191022 ]; then
  echo "Needs sound card reconfiguration"
  rm /newroot/etc/hifiberry.state
 fi
+
+if [ "$V" -lt 20191201 ]; then
+ echo "Version < 20191201, adding postgresql configuration to audiocontrol"
+ echo
+ FOUND=`cat /newroot/etc/audiocontrol2.conf | grep '\[postgres\]'`
+ if [ "$FOUND" == "" ]; then
+  echo >> /newroot/etc/audiocontrol2.conf
+  echo '[postgres]' >> /newroot/etc/audiocontrol2.conf
+  echo "audiocontrol2.conf done"
+ else
+  echo "Postgres already configured"
+ fi
+fi
+
