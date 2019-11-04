2d1f5de4ef768eb66ef2db67653de02776b1897b
HiFiBerry <info@hifiberry.com>
2019-11-04 23:06:02 +0100

    Add more analytics

--- buildroot/package/hifiberry-postgres/hifiberry-postgres.mk
+++ buildroot/package/hifiberry-postgres/hifiberry-postgres.mk
@@ -9,6 +9,8 @@ define HIFIBERRY_POSTGRES_INSTALL_TARGET_CMDS
                 $(TARGET_DIR)/opt/hifiberry/helpers/create-hbdb
         $(INSTALL) -D -m 0555 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-postgres/show-scrobbles \
                 $(TARGET_DIR)/opt/hifiberry/bin/show-scrobbles
+        $(INSTALL) -D -m 0555 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-postgres/show-tags \
+                $(TARGET_DIR)/opt/hifiberry/bin/show-tags
 endef
 
 define HIFIBERRY_POSTGRES_INSTALL_INIT_SYSTEMD
