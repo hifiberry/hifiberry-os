b37977207e2e15fcf1b06ceee64f1d5b74d05ec5
HiFiBerry <info@hifiberry.com>
2019-11-04 23:09:23 +0100

    Add musicbrainz python module

---
+++ buildroot/package/python-musicbrainz-ngs/python-musicbrainz-ngs.mk
@@ -0,0 +1,12 @@
+################################################################################
+#
+# python-musicbrainz-ngs
+#
+################################################################################
+
+PYTHON_MUSICBRAINZ_NGS_VERSION = 0.6
+PYTHON_MUSICBRAINZ_NGS_SOURCE = v$(PYTHON_MUSICBRAINZ_NGS_VERSION).tar.gz
+PYTHON_MUSICBRAINZ_NGS_SITE = https://github.com/alastair/python-musicbrainzngs/archive
+PYTHON_MUSICBRAINZ_NGS_SETUP_TYPE = distutils
+
+$(eval $(python-package))
