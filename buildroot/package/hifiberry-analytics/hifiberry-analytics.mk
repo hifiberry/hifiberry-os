b28259339405ef84f1ac45564f5be58ee02c22f8
HiFiBerry <info@hifiberry.com>
2019-11-06 08:22:15 +0100

    Started working on an anlytics package

---
+++ buildroot/package/hifiberry-analytics/hifiberry-analytics.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# hifiberry-analytics
+#
+################################################################################
+
+define HIFIBERRY_ANALYTICS_INSTALL_TARGET_CMDS
+endef
+
+define HIFIBERRY_ANALYTICS_INSTALL_INIT_SYSTEMD
+endef
+
+$(eval $(generic-package))
