92e71e832aa6ecad7514f19fee29bc1e1621eec9
HiFiBerry <info@hifiberry.com>
2019-11-08 16:02:03 +0100

    Bump to latest version

--- buildroot/package/audiocontrol2/audiocontrol2.mk
+++ buildroot/package/audiocontrol2/audiocontrol2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-AUDIOCONTROL2_VERSION = 1190caf093a75ad9f67fd46aa5066612aa59650a
+AUDIOCONTROL2_VERSION = e741105f7c98c800cdffe9ead07bcfea4ac802d5
 AUDIOCONTROL2_SITE = $(call github,hifiberry,audiocontrol2,$(AUDIOCONTROL2_VERSION))
 
 define AUDIOCONTROL2_BUILD_CMDS
