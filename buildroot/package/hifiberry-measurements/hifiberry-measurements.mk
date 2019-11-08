0e93d70ac9a5c56b37ce95561d258e746ac23815
HiFiBerry <info@hifiberry.com>
2019-11-08 08:41:24 +0100

    Added first version of the measurement tools

--- buildroot/package/hifiberry-measurements/hifiberry-measurements.mk
+++ buildroot/package/hifiberry-measurements/hifiberry-measurements.mk
@@ -5,6 +5,15 @@
 ################################################################################
 
 define HIFIBERRY_MEASUREMENTS_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0555 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-measurements/analyze \
+           $(TARGET_DIR)/opt/hifiberry/bin/fft-analzye
+    $(INSTALL) -D -m 0555 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-measurements/audio-inputs \
+           $(TARGET_DIR)/opt/hifiberry/bin/audio-inputs
+    $(INSTALL) -D -m 0555 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-measurements/record-sweep \
+           $(TARGET_DIR)/opt/hifiberry/bin/record-sweep
+    $(INSTALL) -D -m 0555 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-measurements/room-measure \
+           $(TARGET_DIR)/opt/hifiberry/bin/room-measure
+
 endef
 
 define HIFIBERRY_POSTGRES_INSTALL_INIT_SYSTEMD
