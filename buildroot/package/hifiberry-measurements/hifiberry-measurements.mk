55ed48d3359ad8ba64140b9935181ca5f3d67425
HiFiBerry <info@hifiberry.com>
2019-11-13 15:24:11 +0100

    Add FFT source code, improve measurements

--- buildroot/package/hifiberry-measurements/hifiberry-measurements.mk
+++ buildroot/package/hifiberry-measurements/hifiberry-measurements.mk
@@ -4,8 +4,15 @@
 #
 ################################################################################
 
+define HIFIBERRY_MEASUREMENTS_BUILD_CMDS
+    cd $(@D); \
+      pwd; \
+      cp $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-measurements/analyze/* .; \
+      $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) LDFLAGS=-lm CFLAGS="-mtune=cortex-a7 -mfpu=neon -mfloat-abi=hard -marm -O3 -Wall" all 
+endef
+
 define HIFIBERRY_MEASUREMENTS_INSTALL_TARGET_CMDS
-    $(INSTALL) -D -m 0555 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-measurements/analyze \
+    $(INSTALL) -D -m 0555 $(@D)/analyze \
            $(TARGET_DIR)/opt/hifiberry/bin/fft-analzye
     $(INSTALL) -D -m 0555 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-measurements/audio-inputs \
            $(TARGET_DIR)/opt/hifiberry/bin/audio-inputs
@@ -15,11 +22,11 @@ define HIFIBERRY_MEASUREMENTS_INSTALL_TARGET_CMDS
            $(TARGET_DIR)/opt/hifiberry/bin/room-measure
     $(INSTALL) -D -m 0555 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-measurements/input-level \
            $(TARGET_DIR)/opt/hifiberry/bin/input-level
-    $(INSTALL) -D -m 0555 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-measurements/testtone \
-           $(TARGET_DIR)/opt/hifiberry/bin/testtone
+    $(INSTALL) -D -m 0555 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-measurements/run-measurement \
+           $(TARGET_DIR)/opt/hifiberry/bin/run-measurement
 endef
 
-define HIFIBERRY_POSTGRES_INSTALL_INIT_SYSTEMD
+define HIFIBERRY_MEASUREMENTS_INSTALL_INIT_SYSTEMD
 endef
 
 $(eval $(generic-package))
