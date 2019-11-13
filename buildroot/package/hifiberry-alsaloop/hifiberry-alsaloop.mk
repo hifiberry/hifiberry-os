11ffe5db4fa4d22265cc0f1d85a65a78b43cfbc7
HiFiBerry <info@hifiberry.com>
2019-11-13 15:24:41 +0100

    Don't enable alsaloop by default

--- buildroot/package/hifiberry-alsaloop/hifiberry-alsaloop.mk
+++ buildroot/package/hifiberry-alsaloop/hifiberry-alsaloop.mk
@@ -10,8 +10,8 @@ endef
 define HIFIBERRY_ALSALOOP_INSTALL_INIT_SYSTEMD
         $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-alsaloop/alsaloop.service \
                 $(TARGET_DIR)/usr/lib/systemd/system/alsaloop.service
-        ln -fs ../../../../usr/lib/systemd/system/alsaloop.service \
-                $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/alsaloop.service
+#        ln -fs ../../../../usr/lib/systemd/system/alsaloop.service \
+#                $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/alsaloop.service
 endef
 
 $(eval $(generic-package))
