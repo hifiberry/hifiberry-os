8bbe8374427e7fcc51f740cbbcf5b54aba60ed5e
HiFiBerry <info@hifiberry.com>
2020-01-20 18:16:58 +0100

    Bugfix, baseaddr was hard-coded

--- buildroot/package/dsptoolkit/dump-spdif-status.sh
+++ buildroot/package/dsptoolkit/dump-spdif-status.sh
@@ -2,7 +2,7 @@
 for baseaddr in 0xf61 0xf62 0xf63 0xf64; do
   line="";
   for i in 0 2 4 6 8 a; do
-    j=`dsptoolkit read-hex 0xf62$i`
+    j=`dsptoolkit read-hex $baseaddr$i
     line="$line $j"
   done
   echo $baseaddr $line
