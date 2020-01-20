b3143a83094db3422291f7a343c979e7653fddd1
HiFiBerry <info@hifiberry.com>
2020-01-20 18:17:46 +0100

    Missing quote

--- buildroot/package/dsptoolkit/dump-spdif-status.sh
+++ buildroot/package/dsptoolkit/dump-spdif-status.sh
@@ -2,7 +2,7 @@
 for baseaddr in 0xf61 0xf62 0xf63 0xf64; do
   line="";
   for i in 0 2 4 6 8 a; do
-    j=`dsptoolkit read-hex $baseaddr$i
+    j=`dsptoolkit read-hex $baseaddr$i`
     line="$line $j"
   done
   echo $baseaddr $line
