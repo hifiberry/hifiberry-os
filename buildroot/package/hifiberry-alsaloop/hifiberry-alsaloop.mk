87d37072439e2ff5ccf310654b603b362ba0c488
HiFiBerry <info@hifiberry.com>
2020-01-24 10:53:07 +0000

    Replace alsaloop by a new MPRIS wrapper

--- buildroot/package/hifiberry-alsaloop/hifiberry-alsaloop.mk
+++ buildroot/package/hifiberry-alsaloop/hifiberry-alsaloop.mk
@@ -4,8 +4,17 @@
 #
 ################################################################################
 
-define HIFIBERRY_ALSALOOP_INSTALL_TARGET_CMDS
+HIFIBERRY_ALSALOOP_VERSION = b3f20ae962fa5ff1e582cfff4df0f126e3f1512a
+HIFIBERRY_ALSALOOP_SITE = $(call github,hifiberry,alsaloop,$(HIFIBERRY_ALSALOOP_VERSION))
+
+define SNAPCASTMPRIS_BUILD_CMDS
 endef
+
+define HIFIBERRY_ALSALOOP_INSTALL_TARGET_CMDS
+        mkdir -p $(TARGET_DIR)/opt/alsaloop
+	cp -rv $(@D)/* $(TARGET_DIR)/opt/alsaloop
+endef
+
 
 define HIFIBERRY_ALSALOOP_INSTALL_INIT_SYSTEMD
         $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-alsaloop/alsaloop.service \
