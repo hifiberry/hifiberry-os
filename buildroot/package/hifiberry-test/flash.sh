bc1fad70f76076d7f1d0efdaa8bf5de6a00fbd15
HiFiBerry <info@hifiberry.com>
2020-01-27 10:44:59 +0000

    Added EEPROM flash tool

---
+++ buildroot/package/hifiberry-test/flash.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+GPIO_DRIVER_LOADED=`cat /boot/config.txt | grep i2c-gpio`
+
+if [ "$GPIO_DRIVER_LOADED" == "" ]; then
+  echo "Need to modify config.txt and reboot"
+  echo "dtoverlay=i2c-gpio" >> /boot/config.txt
+  echo "dtparam=i2c_gpio_sda=0" >> /boot/config.txt
+  echo "dtparam=i2c_gpio_scl=1" >> /boot/config.txt
+  echo "Rebooting in 5 seconds"
+  sync
+  sleep 5
+  reboot
+fi
+
+if [ -f "/opt/hifiberry/contrib/$1.eep" ]; then 
+  /opt/hifiberry/contrib/hbflash.sh -w -f=/opt/hifiberry/contrib/$1.eep -t=24c32
+else
+  echo "EEPROM file $f does not exist, aborting ..."
+  exit 1
+fi
+
+echo "Cleaning up /boot/config.txt again"
+cat /boot/config.txt | grep -v i2c_gpio | grep -v i2c-gpio > /tmp/x.conf
+mv /tmp/x.conf /boot/config.txt
+echo "Rebooting in 5 seconds"
+sync
+sleep 5
+reboot
+
