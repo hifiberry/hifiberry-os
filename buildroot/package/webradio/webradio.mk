63f647129ba26f6fdb3dad681525741f084bb051
HiFiBerry <info@hifiberry.com>
2020-02-11 10:00:13 +0000

    Bump to latest version

--- buildroot/package/webradio/webradio.mk
+++ buildroot/package/webradio/webradio.mk
@@ -6,7 +6,7 @@
 
 WEBRADIO=https://github.com/juangaimaro/radio
 
-WEBRADIO_VERSION = 60810e1a1f787975235c0042d2d05c06666aa978
+WEBRADIO_VERSION =  d38cdafe83829262c2bd52db31e3722a4689bf4d
 WEBRADIO_SITE = $(call github,juangaimaro,radio,$(WEBRADIO_VERSION))
 
 define WEBRADIO_INSTALL_TARGET_CMDS
