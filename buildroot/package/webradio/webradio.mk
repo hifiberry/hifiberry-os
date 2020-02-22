bc8bf6acc2b9ee21870a228f4365f1abc3a38dc9
HiFiBerry <info@hifiberry.com>
2020-02-22 06:44:43 +0000

    Minor changes for February release

--- buildroot/package/webradio/webradio.mk
+++ buildroot/package/webradio/webradio.mk
@@ -4,10 +4,11 @@
 #
 ################################################################################
 
-WEBRADIO=https://github.com/juangaimaro/radio
-
 WEBRADIO_VERSION =  d38cdafe83829262c2bd52db31e3722a4689bf4d
 WEBRADIO_SITE = $(call github,juangaimaro,radio,$(WEBRADIO_VERSION))
+
+WEBRADIO_VERSION =8fbd7e5c95e131c0f713950dafe92078bc004d8e
+WEBRADIO_SITE = $(call github,tuomashamalainen,radio,$(WEBRADIO_VERSION))
 
 define WEBRADIO_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0555 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/webradio/start-radio \
