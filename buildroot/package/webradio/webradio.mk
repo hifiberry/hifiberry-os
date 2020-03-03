103b60387e1f8d7688eb728e07b654456c708324
HiFiBerry <info@hifiberry.com>
2020-03-03 13:13:39 +0000

    Resolve radio URLs using the HiFiBerry music web service

--- buildroot/package/webradio/webradio.mk
+++ buildroot/package/webradio/webradio.mk
@@ -7,8 +7,8 @@
 WEBRADIO_VERSION = c5de983f41577007c6eef8501494d6a9cbf30ad6
 WEBRADIO_SITE = $(call github,juangaimaro,radio,$(WEBRADIO_VERSION))
 
-#WEBRADIO_VERSION =8fbd7e5c95e131c0f713950dafe92078bc004d8e
-#WEBRADIO_SITE = $(call github,tuomashamalainen,radio,$(WEBRADIO_VERSION))
+WEBRADIO_VERSION = 38109377aec2bcd6f85671cc986c3a4ce45f5d80
+WEBRADIO_SITE = $(call github,hifiberry,radio,$(WEBRADIO_VERSION))
 
 define WEBRADIO_INSTALL_TARGET_CMDS
 	$(INSTALL) -D -m 0555 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/webradio/start-radio \
