afda5bc0118d9d8989525bf187316d2f166d014a
HiFiBerry <info@hifiberry.com>
2020-03-05 16:20:54 +0000

    Using the HiFIBerry GMediarenderer fork now

--- buildroot/package/hifiberry-gmrender/hifiberry-gmrender.mk
+++ buildroot/package/hifiberry-gmrender/hifiberry-gmrender.mk
@@ -4,15 +4,14 @@
 #
 ################################################################################
 
-HIFIBERRY_GMRENDER_VERSION = 0.0.8
-HIFIBERRY_GMRENDER_SITE = $(call github,hzeller,gmrender-resurrect,v$(GMRENDER_RESURRECT_VERSION))
+HIFIBERRY_GMRENDER_VERSION = master
+HIFIBERRY_GMRENDER_SITE = $(call github,hifiberry,gmrender-resurrect,$(HIFIBERRY_GMRENDER_VERSION))
 # Original distribution does not have default configure,
 # so we need to autoreconf:
 HIFIBERRY_GMRENDER_AUTORECONF = YES
 HIFIBERRY_GMRENDER_LICENSE = GPL-2.0+
 HIFIBERRY_GMRENDER_LICENSE_FILES = COPYING
-HIFIBERRY_GMRENDER_DEPENDENCIES = \
-	gstreamer1 \
-	$(if $(BR2_PACKAGE_LIBUPNP),libupnp,libupnp18)
+HIFIBERRY_GMRENDER_DEPENDENCIES = gstreamer1 libupnp
+HIFIBERRY_GMRENDER_CONF_ENV += LIBS="-lasound"
 
 $(eval $(autotools-package))
