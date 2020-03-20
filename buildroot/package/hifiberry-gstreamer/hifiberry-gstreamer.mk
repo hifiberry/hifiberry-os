4c43816fc6709afdd1e52ed0f8913396a713691a
HiFiBerry <info@hifiberry.com>
2020-03-20 11:39:44 +0000

    Added more Python packages

---
+++ buildroot/package/hifiberry-gstreamer/hifiberry-gstreamer.mk
@@ -0,0 +1,20 @@
+################################################################################
+#
+# hifiberry-gstreamer
+#
+################################################################################
+
+HIFIBERRY_GSTREAMER_DEPENDENCIES += gstreamer1 gst1-plugins-base gst1-plugins-good gst1-plugins-bad gst1-plugins-ugly
+
+define HIFIBERRY_GSTREAMER_INSTALL_EXTRA_FILES
+	echo "Hack: Copying compiled typelib files"
+	if [ ! -f $(TARGET_DIR)/usr/lib/girepository-1.0 ]; then
+		mkdir $(TARGET_DIR)/usr/lib/girepository-1.0
+	fi
+	cp $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-ghstreamer/typelib/* \
+        	$(TARGET_DIR)/usr/lib/girepository-1.0
+endef
+
+HIFIBERRY_GSTREAMER_POST_INSTALL_TARGET_HOOKS += HIFIBERRY_GSTREAMER_INSTALL_EXTRA_FILES
+
+$(eval $(generic-package))
