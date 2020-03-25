cbbaf16d6918489f7032f4d9c8656e9e88b7a8df
HiFiBerry <info@hifiberry.com>
2020-03-25 08:46:05 +0000

    Changes for new buildroot version

--- buildroot/package/disable-samba/disable-samba.mk
+++ buildroot/package/disable-samba/disable-samba.mk
@@ -4,18 +4,21 @@
 #
 ################################################################################
 
-define DISABLE_SAMBA_INSTALL_TARGET_CMDS
+define DISABLE_SAMBA_INSTALL_IMAGES_CMDS
  	echo "Removing SAMBA services"
-	if [ -f $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/smb.service ]; then \
-		rm $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/smb.service; \
-	fi
-	if [ -f $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/nmb.service ]; then \
-		rm $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/nmb.service; \
-	fi
-	if [ -f $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/winbind.service ]; then \
-		rm $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/winbind.service; \
-        fi
+	for s in smb nmb winbind samba; do \
+        	if [ -h $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/$$s.service ]; then \
+                	echo "removing $$s.service in /etc/systemd/system/multi-user.target.wants"; \
+                	rm $(TARGET_DIR)/etc/systemd/system/multi-user.target.wants/$$s.service; \
+		fi; \
+		if [ -f $(TARGET_DIR)/lib/systemd/system/$$s.service ]; then \
+                        echo "removing $$s.service in /lib/systems/system"; \
+                        rm $(TARGET_DIR)/lib/systemd/system/$$s.service; \
+                fi; \
+	done
 endef
+
+DISABLE_SAMBA_INSTALL_TARGET_CMDS = $(DISABLE_SAMBA_INSTALL_IMAGES_CMDS)
 
 $(eval $(generic-package))
 
