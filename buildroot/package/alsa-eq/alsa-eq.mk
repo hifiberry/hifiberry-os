b2888f40db173df423483d307cb2e8c67df2427b
HiFiBerry <info@hifiberry.com>
2020-04-03 12:17:29 +0000

    Add ALSA equaliser

---
+++ buildroot/package/alsa-eq/alsa-eq.mk
@@ -0,0 +1,25 @@
+#############################################################
+#
+# alsa-eq
+#
+#############################################################
+ALSA_EQ_VERSION = 0.6
+ALSA_EQ_SOURCE = alsaequal-$(ALSA_EQ_VERSION).tar.bz2
+ALSA_EQ_SITE = https://thedigitalmachine.net/tools
+ALSA_EQ_DEPENDENCIES = caps
+
+define ALSA_EQ_BUILD_CMDS
+	cd $(@D) && CC=$(TARGET_CC) LD=$(TARGET_CC) make
+	echo "alsaeq make done"
+	sleep 1
+endef
+
+
+define ALSA_EQ_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 755 $(@D)/libasound_module_ctl_equal.so \
+           $(TARGET_DIR)/usr/lib/alsa-lib/
+	$(INSTALL) -D -m 755 $(@D)/libasound_module_pcm_equal.so \
+           $(TARGET_DIR)/usr/lib/alsa-lib/
+endef
+
+$(eval $(generic-package))
