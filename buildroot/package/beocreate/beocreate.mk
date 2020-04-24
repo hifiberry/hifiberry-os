941a63dcb33ed23eda57ccb03c299d4b59971787
HiFiBerry <info@hifiberry.com>
2020-04-24 15:59:42 +0000

    Bump to latest version

--- buildroot/package/beocreate/beocreate.mk
+++ buildroot/package/beocreate/beocreate.mk
@@ -4,11 +4,11 @@
 #
 ################################################################################
 
-BEOCREATE_VERSION = 8ed19b69a2f3e7d5840dbb3f9af7f59d7dfcda79
+BEOCREATE_VERSION = 8579f01d29f91be3711a0dceeb4134394067949c
 BEOCREATE_SITE = $(call github,bang-olufsen,create,$(BEOCREATE_VERSION))
 
-BEOCREATE_VERSION = ea520f8086f5639a94f81375d52492b1b66d273b
-BEOCREATE_SITE = $(call github,hifiberry,create,$(BEOCREATE_VERSION))
+#BEOCREATE_VERSION = ea520f8086f5639a94f81375d52492b1b66d273b
+#BEOCREATE_SITE = $(call github,hifiberry,create,$(BEOCREATE_VERSION))
 
 define BEOCREATE_BUILD_CMDS
 endef
