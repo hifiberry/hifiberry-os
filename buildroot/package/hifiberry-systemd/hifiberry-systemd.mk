9eea4cf4eafe74909ba88af8417cb2694b50422d
HiFiBerry <info@hifiberry.com>
2020-05-23 17:45:45 +0000

    Make sure directories are created before installing

--- buildroot/package/hifiberry-systemd/hifiberry-systemd.mk
+++ buildroot/package/hifiberry-systemd/hifiberry-systemd.mk
@@ -6,8 +6,8 @@
 
 define HIFIBERRY_SYSTEMD_INSTALL_TARGET_CMDS
  	echo "Speed up network-online"
-	sed -i "s/ExecStart.*/ExecStart=\/usr\/lib\/systemd\/systemd-networkd-wait-online\ --timeout=20/" \
-		$(TARGET_DIR)/usr/lib/systemd/system/systemd-networkd-wait-online.service
+	#sed -i "s/ExecStart.*/ExecStart=\/usr\/lib\/systemd\/systemd-networkd-wait-online\ --timeout=20/" \
+	#	$(TARGET_DIR)/usr/lib/systemd/system/systemd-networkd-wait-online.service
 endef
 
 $(eval $(generic-package))
