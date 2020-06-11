bc5191f47a7421379c2b0160a601faa8d6396d8b
HiFiBerry <info@hifiberry.com>
2020-06-11 13:30:39 +0000

    Bugfix when creating directories

--- buildroot/package/hifiberry-automount/mount-smb.sh
+++ buildroot/package/hifiberry-automount/mount-smb.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
+BASEDIR=/data/library/music
 for m in `cat /etc/smbmounts.conf | grep -v ^#`; do
- dir=`echo $m | awk -F, '{print $1}'`
- mkdir -p $dir
- mountcmd=`echo $m | awk -F, '{print "mount -t cifs -o user=" $3 ",password=" $4 " " $2 " /data/library/music/" $1}'`
- ${mountcmd}
+  dir=`echo $m | awk -F, '{print $1}'`
+  if [ ! -d $BASEDIR/$dir ]; then
+    mkdir -p $BASEDIR/$dir
+  fi
+  mountcmd=`echo $m | awk -F, '{print "mount -t cifs -o user=" $3 ",password=" $4 " " $2 " /data/library/music/" $1}'`
+  ${mountcmd}
 done
 
