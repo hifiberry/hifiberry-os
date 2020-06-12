ee4e31c4fef99e4436ac7d17d2d73562b3fdadf8
HiFiBerry <info@hifiberry.com>
2020-06-12 15:32:35 +0000

    Bump to latest versions

--- buildroot/package/musicfiletools/musicfiletools.mk
+++ buildroot/package/musicfiletools/musicfiletools.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-MUSICFILETOOLS_VERSION = a291fc022a95b5d2bf9938e969e5b8d6acd9c5df
+MUSICFILETOOLS_VERSION = 48f31a2eeacb0078dc32442336f9b9bdc3ee8d1f
 MUSICFILETOOLS_SITE = $(call github,hifiberry,musicfiletools,$(MUSICFILETOOLS_VERSION))
 MUSICFILETOOLS_SETUP_TYPE = setuptools
 MUSICFILETOOLS_LICENSE = MIT
