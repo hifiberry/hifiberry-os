cfb6833ee6637586e4f0a1edcfab9b71fb69817c
HiFiBerry <info@hifiberry.com>
2020-07-02 12:45:31 +0000

    Allow additional mount options

--- buildroot/package/hifiberry-automount/mount-smb.sh
+++ buildroot/package/hifiberry-automount/mount-smb.sh
@@ -13,7 +13,9 @@ for m in `cat /etc/smbmounts.conf | grep -v ^#`; do
       m=`echo $m | sed s/$HOST/$IP/`
     fi
   fi  
-  mountcmd=`echo $m | awk -F, '{print "mount -t cifs -o user=" $3 ",password=" $4 " " $2 " /data/library/music/" $1}'`
+  
+  mountcmd=`echo $m | awk -F, '$5=="" { $5="rw" } {print "mount -t cifs -o user=" $3 ",password=" $4 "," $5 " " $2 " /data/library/music/" $1}'`
+  echo $mountcmd
   ${mountcmd}
 done
 if [ -x /opt/hifiberry/bin/update-mpd-db ]; then
