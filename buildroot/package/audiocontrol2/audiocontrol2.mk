30cb0eee9354635ecb39efacac9bc3e3ac75efe5
HiFiBerry <info@hifiberry.com>
2020-07-02 20:45:37 +0000

    Bug fixes

--- buildroot/package/audiocontrol2/audiocontrol2.mk
+++ buildroot/package/audiocontrol2/audiocontrol2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-AUDIOCONTROL2_VERSION = e999e57868be732f110fe431ce9c98249ccb6990
+AUDIOCONTROL2_VERSION = 0cbe8f3c273f023227f4ee8af92862891a90165a
 AUDIOCONTROL2_SITE = $(call github,hifiberry,audiocontrol2,$(AUDIOCONTROL2_VERSION))
 
 AUDIOCONTROL2_DEPENDENCIES = python3
--- buildroot/package/beocreate/beocreate.mk
+++ buildroot/package/beocreate/beocreate.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BEOCREATE_VERSION = 78393b54f96868f04e7b571432c51b3287cbcea3
+BEOCREATE_VERSION =  4f80355c20fbc6dada8ba4690002148a5dae11b9
 BEOCREATE_SITE = $(call github,bang-olufsen,create,$(BEOCREATE_VERSION))
 
 #BEOCREATE_VERSION = ea520f8086f5639a94f81375d52492b1b66d273b
