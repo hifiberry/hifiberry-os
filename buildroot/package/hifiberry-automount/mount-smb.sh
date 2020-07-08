73c87c31547bc68cadc9d3a8c53c8d87f1a2d79e
HiFiBerry <info@hifiberry.com>
2020-07-08 14:37:05 +0200

    Update mount-smb.sh

--- buildroot/package/hifiberry-automount/mount-smb.sh
+++ buildroot/package/hifiberry-automount/mount-smb.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 BASEDIR=/data/library/music
 for m in `cat /etc/smbmounts.conf | grep -v ^#`; do
-  dir=`echo $m | awk -F, '{print $1}'`
+  dir=`echo $m | awk -F\; '{print $1}'`
   if [ ! -d $BASEDIR/$dir ]; then
     mkdir -p $BASEDIR/$dir
   fi
   # Check if share is on a .local host, resolve this first
-  HOST=`echo $m | awk -F, '{print $2}' | awk -F\/ '{print $3}'`
+  HOST=`echo $m | awk -F\; '{print $2}' | awk -F\/ '{print $3}'`
   if [[ $HOST == *.local ]]; then 
     IP=`avahi-resolve-host-name $HOST | awk '{print $2}'`
     if [ "$IP" != "" ]; then
