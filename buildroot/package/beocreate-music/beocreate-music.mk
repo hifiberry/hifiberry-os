fa17e561354252dcc29698616730bee6e3ef902d
HiFiBerry <info@hifiberry.com>
2020-07-19 20:13:29 +0000

    Bump

--- buildroot/package/beocreate-music/beocreate-music.mk
+++ buildroot/package/beocreate-music/beocreate-music.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-BEOCREATE_MUSIC_VERSION = f87485bb8b2b52caae982d1932b4f6a0dfc11b42
+BEOCREATE_MUSIC_VERSION = f6526e91cc795dd2d75b223a74dce4e9d5437f9e
 BEOCREATE_MUSIC_SITE = $(call github,tuomashamalainen,beocreate-music,$(BEOCREATE_MUSIC_VERSION))
 
 define BEOCREATE_MUSIC_INSTALL_TARGET_CMDS
