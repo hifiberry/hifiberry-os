c8d9788ba8fc984c3e92a7207ba273a3a998f7c3
HiFiBerry <info@hifiberry.com>
2020-09-08 12:25:41 +0000

    Added i2c-gpio (for firmware updater)

--- buildroot/package/copy-overlays/copy-overlays.mk
+++ buildroot/package/copy-overlays/copy-overlays.mk
@@ -10,7 +10,7 @@ COPY_OVERLAYS_DEPENDENCIES = rpi-firmware linux
 define COPY_OVERLAYS_INSTALL_TARGET_CMDS
 	echo "Copying overlays"
 	mkdir -p $(TARGET_DIR)/usr/lib/firmware/rpi/overlays
-        for i in hifiberry vc4; do \
+        for i in hifiberry vc4 i2c-gpio; do \
 	  	cp -v $(BUILD_DIR)/linux-custom/arch/arm/boot/dts/overlays/$$i*.dtbo $(BINARIES_DIR)/rpi-firmware/overlays; \
                 cp -v $(BUILD_DIR)/linux-custom/arch/arm/boot/dts/overlays/$$i*.dtbo $(TARGET_DIR)/usr/lib/firmware/rpi/overlays; \
         done
