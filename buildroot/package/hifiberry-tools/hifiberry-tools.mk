0fb8d3bac15372c61e5f8eee3e2d46ff38b8de78
HiFiBerry <info@hifiberry.com>
2020-10-12 14:06:04 +0000

    Improved testing

--- buildroot/package/hifiberry-tools/hifiberry-tools.mk
+++ buildroot/package/hifiberry-tools/hifiberry-tools.mk
@@ -49,6 +49,8 @@ define HIFIBERRY_TOOLS_INSTALL_TARGET_CMDS
                 $(TARGET_DIR)/opt/hifiberry/bin
     $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-tools/check-system \
                 $(TARGET_DIR)/opt/hifiberry/bin
+    $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-tools/unmute-amp100 \
+                $(TARGET_DIR)/opt/hifiberry/bin
     touch $(TARGET_DIR)/resize-me
     touch $(TARGET_DIR)/etc/spotifyd.conf
 
