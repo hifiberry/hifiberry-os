b5672523c96dd29f33a534843eb4777adc961bb8
HiFiBerry <info@hifiberry.com>
2020-11-02 06:26:43 +0000

    Bump

--- buildroot/package/audiocontrol2/audiocontrol2.mk
+++ buildroot/package/audiocontrol2/audiocontrol2.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-AUDIOCONTROL2_VERSION = 625f7fd826911657337398d4b09da4c3e14afb09
+AUDIOCONTROL2_VERSION = 16cc56abbb90357df08c5bcd1e83b6d232b94ef1
 AUDIOCONTROL2_SITE = $(call github,hifiberry,audiocontrol2,$(AUDIOCONTROL2_VERSION))
 
 AUDIOCONTROL2_DEPENDENCIES = python3
