a14fe96868ae1815b14e81762322fc7dfefee0e0
HiFiBerry <info@hifiberry.com>
2020-11-11 12:14:43 +0000

    Fix for CVE-2020-12695
    https://de.tenable.com/blog/cve-2020-12695-callstranger-vulnerability-in-universal-plug-and-play-upnp-puts-billions-of

--- buildroot/package/hifiberry-gmrender/hifiberry-gmrender.mk
+++ buildroot/package/hifiberry-gmrender/hifiberry-gmrender.mk
@@ -4,7 +4,8 @@
 #
 ################################################################################
 
-HIFIBERRY_GMRENDER_VERSION = bfd6d4e251e69169c6920e62cc041e5cf32fef79
+#HIFIBERRY_GMRENDER_VERSION = bfd6d4e251e69169c6920e62cc041e5cf32fef79
+HIFIBERRY_GMRENDER_VERSION = 20a6928fab503fbbfbb2e80a9c1c4419f11777db # UpnpInit2 patch
 HIFIBERRY_GMRENDER_SITE = $(call github,hifiberry,gmrender-resurrect,$(HIFIBERRY_GMRENDER_VERSION))
 # Original distribution does not have default configure,
 # so we need to autoreconf:
