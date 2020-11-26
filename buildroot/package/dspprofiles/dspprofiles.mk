60187d33e2e4e9d05097d1c272c6cad0edc2ccc4
HiFiBerry <info@hifiberry.com>
2020-11-26 11:15:25 +0000

    Bugfix: version number incorrect during copy/paste

--- buildroot/package/dspprofiles/dspprofiles.mk
+++ buildroot/package/dspprofiles/dspprofiles.mk
@@ -4,7 +4,6 @@
 #
 ################################################################################
 
-DSP_PROFILE_VERSION=10
 define DSPPROFILES_INSTALL_TARGET_CMDS
      -mkdir -p $(TARGET_DIR)/opt/beocreate/beo-dsp-programs
      -mkdir -p $(TARGET_DIR)/opt/hifiberry/contrib
@@ -14,7 +13,7 @@ define DSPPROFILES_INSTALL_TARGET_CMDS
      $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/dspprofiles/dspdac-11.xml \
                 $(TARGET_DIR)/opt/beocreate/beo-dsp-programs/dacdsp-11.xml
      $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/dspprofiles/dsp-addon-96-11.xml \
-                $(TARGET_DIR)/opt/beocreate/beo-dsp-programs/dsp-addon-96-12.xml
+                $(TARGET_DIR)/opt/beocreate/beo-dsp-programs/dsp-addon-96-11.xml
      $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/dspprofiles/dsp-addon-96-12.xml \
 		$(TARGET_DIR)/opt/beocreate/beo-dsp-programs/dsp-addon-96-12.xml
 
