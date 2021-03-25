55512c4246d1929ddf0650f7abc66923dc6208e4
Tony Tromp <tony@eureka.ai>
2021-03-25 07:36:18 +0100

    Fixed mount-smb.sh for NFS shares with spaces

--- mount-smb.sh
+++ mount-smb.sh
@@ -13,22 +13,19 @@ while read -r line; do
   PASSWORD=$(echo $PASSWORD|tr -d '\n')
   MOUNTOPTS=$(echo $MOUNTOPTS|tr -d '\n')
 
-  echo "DEBUG"
-  echo $USER
   echo "mountid=${MOUNTID}, share=${SHARE}, user=${USER}, passwd=${PASSWORD}, mountopts=${MOUNTOPTS}"
 
   if [ "$MOUNTOPTS" == "" ]; then
     MOUNTOPTS="rw"
   fi
 
-  if [ ! -d '$BASEDIR/$MOUNTID' ]; then
-    mkdir -p '$BASEDIR/$MOUNTID'
+  if [ ! -d $BASEDIR/$MOUNTID ]; then
+    mkdir -p "$BASEDIR/$MOUNTID"
   fi
 
   # Removed local resolving as it leads to SHARE beeing empty if not resolvable
-  mountcmd="mount.cifs '$SHARE' '/data/library/music/$MOUNTID' -o user=$USER,password=$PASSWORD,$MOUNTOPTS"
-  echo ${mountcmd}
-  ${mountcmd}
+  mount.cifs "${SHARE}" "/data/library/music/${MOUNTID}" -o user=$USER,password=$PASSWORD,$MOUNTOPTS
+  echo "${SHARE} mounted on /data/library/music/${MOUNTID}"
 
   if [ -x /opt/hifiberry/bin/report-activation ]; then
     /opt/hifiberry/bin/report-activation mount_samba
@@ -37,7 +34,7 @@ done < /etc/smbmounts.conf
 
 if [ "$1" != "--no-update" ]; then
   if [ -x /opt/hifiberry/bin/update-mpd-db ]; then
-    /opt/hifiberry/bin/update-mpd-db &
+    /opt/hifiberry/bin/update-mpd-db
   fi
 fi
 
