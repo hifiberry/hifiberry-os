bdd50118fed0c71fb65a3f3fb4c0954f22572472
HiFiBerry <info@hifiberry.com>
2021-04-08 09:16:14 +0000

    Fixes in power controller integration

--- buildroot/package/hifiberry-powercontroller/hifiberry-powercontroller.mk
+++ buildroot/package/hifiberry-powercontroller/hifiberry-powercontroller.mk
@@ -11,8 +11,9 @@ PC_DOWNLOAD=https://github.com/hifiberry/powercontroller/releases/download/$(PC_
 define HIFIBERRY_POWERCONTROLLER_INSTALL_TARGET_CMDS
     $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-powercontroller/pc-write \
                 $(TARGET_DIR)/opt/hifiberry/bin
-    mkdir -p $(TARGET_DIR)/opt/hifiberry/powercontroller
-    curl $(PC_DOWNLOAD) --output $(TARGET_DIR)/opt/hifiberry/powercontroller/firmware-$(PC_VERSION).hex
+    $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-powercontroller/write-firmware \
+                $(TARGET_DIR)/opt/hifiberry/powercontroller/write-firmware
+    curl -L $(PC_DOWNLOAD) --output $(TARGET_DIR)/opt/hifiberry/powercontroller/firmware-$(PC_VERSION).hex
 endef
 
 define HIFIBERRY_POWERCONTROLLER_INSTALL_INIT_SYSTEMD
