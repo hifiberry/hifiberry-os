38a5f4c8d94a796a561987591f1bfe9bcd922a7b
HiFiBerry <info@hifiberry.com>
2022-02-22 14:18:10 +0000

    Bump to latest version

--- buildroot/package/python-musicbrainz-ngs/python-musicbrainz-ngs.mk
+++ buildroot/package/python-musicbrainz-ngs/python-musicbrainz-ngs.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_MUSICBRAINZ_NGS_VERSION = 0.6
+PYTHON_MUSICBRAINZ_NGS_VERSION = 0.7.1
 PYTHON_MUSICBRAINZ_NGS_SOURCE = v$(PYTHON_MUSICBRAINZ_NGS_VERSION).tar.gz
 PYTHON_MUSICBRAINZ_NGS_SITE = https://github.com/alastair/python-musicbrainzngs/archive
 PYTHON_MUSICBRAINZ_NGS_SETUP_TYPE = distutils
