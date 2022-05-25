f2bf5cf7bfb7d3bd6f8b74421e94f6c2f4fb41a7
HiFiBerry <info@hifiberry.com>
2022-05-25 11:58:35 +0000

    Try a newer version

--- buildroot/package/python-pymcuprog/python-pymcuprog.mk
+++ buildroot/package/python-pymcuprog/python-pymcuprog.mk
@@ -4,7 +4,8 @@
 #
 ################################################################################
 
-PYTHON_PYMCUPROG_VERSION = dfa0f9b4352c41dbd58d32f3f091517112a1908c
+#PYTHON_PYMCUPROG_VERSION = dfa0f9b4352c41dbd58d32f3f091517112a1908c
+PYTHON_PYMCUPROG_VERSION = 44c2e6e11e1f8b5246627d01283996784c6797ee
 PYTHON_PYMCUPROG_SITE = $(call github,microchip-pic-avr-tools,pymcuprog,$(PYTHON_PYMCUPROG_VERSION))
 PYTHON_PYMCUPROG_SETUP_TYPE = setuptools
 
