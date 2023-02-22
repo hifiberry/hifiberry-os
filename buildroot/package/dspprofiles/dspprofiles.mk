12f4463ed09df63f29fef1423058bd7da20379dd
HiFiBerry <info@hifiberry.com>
2023-02-22 07:36:29 +0000

    Added DAC+ DSP profile v13 (fixed timings for latest hardware)

--- buildroot/package/dspprofiles/dspprofiles.mk
+++ buildroot/package/dspprofiles/dspprofiles.mk
@@ -18,6 +18,8 @@ define DSPPROFILES_INSTALL_TARGET_CMDS
                 $(TARGET_DIR)/opt/beocreate/beo-dsp-programs/dacdsp-11.xml
      $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/dspprofiles/dspdac-12.xml \
                 $(TARGET_DIR)/opt/beocreate/beo-dsp-programs/dacdsp-12.xml
+     $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/dspprofiles/dspdac-13.xml \
+                $(TARGET_DIR)/opt/beocreate/beo-dsp-programs/dacdsp-13.xml
      $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/dspprofiles/dsp-addon-96-11.xml \
                 $(TARGET_DIR)/opt/beocreate/beo-dsp-programs/dsp-addon-96-11.xml
      $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/dspprofiles/dsp-addon-96-13.xml \
