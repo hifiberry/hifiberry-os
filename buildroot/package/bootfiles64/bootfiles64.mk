576fc300bff5a77d9c16e5c50e98c6a251f8b07b
HiFiBerry <info@hifiberry.com>
2023-12-19 15:54:36 +0000

    Copy files to the correct directories

--- buildroot/package/bootfiles64/bootfiles64.mk
+++ buildroot/package/bootfiles64/bootfiles64.mk
@@ -8,7 +8,7 @@ BOOTFILES64_VERSION = 7e6decce72fdff51923e9203db46716835ae889a
 BOOTFILES64_SITE = $(call github,raspberrypi,firmware,$(BOOTFILES64_VERSION))
 BOOTFILES64_DEPENDENCIES = rpi-firmware
 
-BOOTFILES64_FILES = \
+BOOTFILES64_DTB_FILES = \
    bcm2710-rpi-3-b-plus.dtb \
    bcm2710-rpi-3-b.dtb \
    bcm2710-rpi-cm3.dtb \
@@ -18,7 +18,8 @@ BOOTFILES64_FILES = \
    bcm2711-rpi-cm4-io.dtb \
    bcm2711-rpi-cm4.dtb \
    bcm2711-rpi-cm4s.dtb \
-   bcm2712-rpi-5-b.dtb \
+   bcm2712-rpi-5-b.dtb 
+BOOTFILES64_FW_FILES = \
    bootcode.bin \
    fixup.dat \
    fixup4.dat \
@@ -30,12 +31,14 @@ BOOTFILES64_FILES = \
    start_x.elf
 
 define BOOTFILES64_INSTALL_TARGET_CMDS
- echo "Hello world"
- for file in $(BOOTFILES64_FILES); do \
+ for file in $(BOOTFILES64_DTB_FILES); do \
+	echo $(BINARIES_DIR)/$$file; \
         $(INSTALL) -D -m 0644 $(@D)/boot/$$file $(BINARIES_DIR)/$$file; \
  done
- $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/bootfiles64/genimage.cfg \
-                $(BINARIES_DIR)/
+ for file in $(BOOTFILES64_FW_FILES); do \
+        echo $(BINARIES_DIR)/$$file; \
+        $(INSTALL) -D -m 0644 $(@D)/boot/$$file $(BINARIES_DIR)/rpi-firmware/$$file; \
+ done
 endef
 
 $(eval $(generic-package))
