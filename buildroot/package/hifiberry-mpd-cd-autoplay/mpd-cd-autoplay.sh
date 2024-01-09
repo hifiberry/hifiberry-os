b4aa4ed6021b93f0193585207ae7fa028eabcfbf
Ben Weston <35574350+bweston6@users.noreply.github.com>
2024-01-09 19:45:17 +0000

    Create cd autoplay service using mpd.

---
+++ buildroot/package/hifiberry-mpd-cd-autoplay/mpd-cd-autoplay.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+ACTION="$1"
+if [[ -z ${ACTION} ]]; then
+ exit 1
+fi
+
+DEV="/dev/sr0"
+MPC="/usr/bin/mpc"
+
+do_mount() {
+	NUM_TRACK=$(/usr/bin/udevadm info --query=property ${DEV} | /bin/grep ID_CDROM_MEDIA_TRACK_COUNT_AUDIO | /usr/bin/awk -F= '{ print $2 }')
+	/bin/echo "cd with ${NUM_TRACK} tracks detected"
+ 
+	/bin/echo "clearing mpd queue"
+	${MPC} clear
+
+	for i in $(/usr/bin/seq 1 ${NUM_TRACK}); do
+		${MPC} add cdda:///${i}
+	done
+
+	${MPC} play
+}
+
+do_unmount() {
+	${MPC} -f "%position% %file%" playlist | /bin/grep cdda:// | /usr/bin/awk '{ print $1 }' | ${MPC} del
+}
+
+case "${ACTION}" in
+	add)
+	do_mount
+	;;
+	remove)
+	do_unmount
+	;;
+esac
