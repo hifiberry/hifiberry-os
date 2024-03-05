ffc75d50dbace3d5a2d44c9f96eb8c83fa7006d8
HiFiBerry <info@hifiberry.com>
2024-03-05 15:41:05 +0000

    Remove default overlays

--- buildroot/package/copy-overlays/copy-overlays.mk
+++ buildroot/package/copy-overlays/copy-overlays.mk
@@ -7,25 +7,10 @@
 
 COPY_OVERLAYS_DEPENDENCIES = rpi-firmware linux
 
-ifdef HIFIBERRY_PIVERSION_0w
-PIOVERLAYS = bcm2708-rpi-zero-w.dtb
-endif
-
-ifdef HIFIBERRY_PIVERSION_2
-PIOVERLAYS = bcm2709-rpi-2-b.dtb
-endif
-
-ifdef HIFIBERRY_PIVERSION_3
-PIOVERLAYS = bcm2710-rpi-3-b-plus.dtb bcm2710-rpi-3-b.dtb bcm2710-rpi-cm3.dtb
-endif
-
-ifdef HIFIBERRY_PIVERSION_4
-PIOVERLAYS = bcm2711-rpi-4-b.dtb
-endif
-
 define COPY_OVERLAYS_INSTALL_TARGET_CMDS
 	# Copy overlays
 	mkdir -p $(TARGET_DIR)/usr/lib/firmware/rpi/overlays
+	rm $(BINARIES_DIR)/rpi-firmware/overlays/*
         for i in hifiberry vc4 i2c-gpio gpio-ir cma dwc disable rpi- spi i2c uart; do \
 	  	cp -v $(BUILD_DIR)/linux-custom/arch/arm/boot/dts/overlays/$$i*.dtbo $(BINARIES_DIR)/rpi-firmware/overlays; \
                 cp -v $(BUILD_DIR)/linux-custom/arch/arm/boot/dts/overlays/$$i*.dtbo $(TARGET_DIR)/usr/lib/firmware/rpi/overlays; \
