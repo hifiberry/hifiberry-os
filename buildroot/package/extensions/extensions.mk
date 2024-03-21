d1d799e9e2e15c27140360d9e1cbf2f7776b421e
HiFiBerry <info@hifiberry.com>
2024-03-21 07:51:30 +0000

    Move shairport-sync to extensions

--- buildroot/package/extensions/extensions.mk
+++ buildroot/package/extensions/extensions.mk
@@ -18,13 +18,13 @@ define EXTENSIONS_INSTALL_TARGET_CMDS
     chmod +x $(TARGET_DIR)/opt/extensions/extensions.py
     -rm $(TARGET_DIR)/opt/hifiberry/bin/extensions
     ln -s ../../extensions/extensions.py $(TARGET_DIR)/opt/hifiberry/bin/extensions
-	$(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/extensions/extensions.conf \
-		$(TARGET_DIR)/etc/extensions.conf
+    $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/extensions/extensions.conf \
+        $(TARGET_DIR)/etc/extensions.conf
 endef
 
 define EXTENSIONS_INSTALL_INIT_SYSTEMD
-        $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/extensions/extensions.service \
-                $(TARGET_DIR)/lib/systemd/system/extensions.service
+    $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/extensions/extensions.service \
+        $(TARGET_DIR)/lib/systemd/system/extensions.service
 endef
 
 
