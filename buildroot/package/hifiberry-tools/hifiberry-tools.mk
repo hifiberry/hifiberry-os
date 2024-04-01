015daccbf1b9c146342bd96aeb9642f1deef1d31
HiFiBerry <info@hifiberry.com>
2024-04-01 19:50:09 +0000

    Add reset-system script

--- buildroot/package/hifiberry-tools/hifiberry-tools.mk
+++ buildroot/package/hifiberry-tools/hifiberry-tools.mk
@@ -73,6 +73,8 @@ define HIFIBERRY_TOOLS_INSTALL_TARGET_CMDS
                 $(TARGET_DIR)/opt/hifiberry/bin
     $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-tools/check-network.sh \
 	        $(TARGET_DIR)/opt/hifiberry/bin
+    $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-tools/reset-system \
+	        $(TARGET_DIR)/opt/hifiberry/bin
     $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-tools/snd_soc_core_disable_pm.conf \
 		$(TARGET_DIR)/etc/modprobe.d/snd_soc_core_disable_pm.conf
     touch $(TARGET_DIR)/resize-me
