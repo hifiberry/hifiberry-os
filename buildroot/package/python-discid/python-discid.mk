e7b26b4dfed1bd944f3a6acaf591397ac409e76f
Ben Weston <35574350+bweston6@users.noreply.github.com>
2024-05-01 16:47:26 +0100

    Use new setuptools instead of outdated buildtools.

--- buildroot/package/python-discid/python-discid.mk
+++ buildroot/package/python-discid/python-discid.mk
@@ -6,6 +6,6 @@
 
 PYTHON_DISCID_VERSION = v1.2.0
 PYTHON_DISCID_SITE = $(call github,JonnyJD,python-discid,$(PYTHON_DISCID_VERSION))
-PYTHON_DISCID_SETUP_TYPE = distutils
+PYTHON_DISCID_SETUP_TYPE = setuptools
 
 $(eval $(python-package))
