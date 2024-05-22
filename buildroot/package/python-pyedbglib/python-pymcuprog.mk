0350ab33134e082ec0c12edc580044ed52f610af
HiFiBerry <info@hifiberry.com>
2024-05-22 08:06:35 +0000

    Update microchip programmer

--- buildroot/package/python-pyedbglib/python-pymcuprog.mk
+++ buildroot/package/python-pyedbglib/python-pymcuprog.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PYTHON_PYEDBGLIB_VERSION = b91b24ab19a6557a67dea9082616650c20c7a01f
+PYTHON_PYEDBGLIB_VERSION = 9bbeceba942772ef31b9c059b761460a782313e6
 PYTHON_PYEDBGLIB_SITE = $(call github,microchip-pic-avr-tools,pyedbglib,$(PYTHON_PYEDBGLIB_VERSION))
 PYTHON_PYEDBGLIB_SETUP_TYPE = setuptools
 
--- buildroot/package/python-pymcuprog/python-pymcuprog.mk
+++ buildroot/package/python-pymcuprog/python-pymcuprog.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 #PYTHON_PYMCUPROG_VERSION = dfa0f9b4352c41dbd58d32f3f091517112a1908c
-PYTHON_PYMCUPROG_VERSION = 44c2e6e11e1f8b5246627d01283996784c6797ee
+PYTHON_PYMCUPROG_VERSION = dbd4904c8208797a05c4beb6649a3bd58f2ae662
 PYTHON_PYMCUPROG_SITE = $(call github,microchip-pic-avr-tools,pymcuprog,$(PYTHON_PYMCUPROG_VERSION))
 PYTHON_PYMCUPROG_SETUP_TYPE = setuptools
 
