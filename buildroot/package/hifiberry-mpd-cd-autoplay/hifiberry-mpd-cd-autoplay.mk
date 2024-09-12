de85f25c48dc7b2b812d19835a4f24d2e19adfd3
Ben Weston <35574350+bweston6@users.noreply.github.com>
2024-09-12 15:23:48 +0100

    Add udev rule to initialise Apple Superdrives

--- buildroot/package/hifiberry-mpd-cd-autoplay/hifiberry-mpd-cd-autoplay.mk
+++ buildroot/package/hifiberry-mpd-cd-autoplay/hifiberry-mpd-cd-autoplay.mk
@@ -4,13 +4,15 @@
 #
 ################################################################################
 
-HIFIBERRY_MPD_CD_AUTOPLAY_VERSION = 0.0.1
+HIFIBERRY_MPD_CD_AUTOPLAY_VERSION = 0.0.2
 HIFIBERRY_MPD_CD_AUTOPLAY_LICENSE = GPL-3.0+
 HIFIBERRY_MPD_CD_AUTOPLAY_LICENSE_FILES = COPYING
 HIFIBERRY_MPD_CD_AUTOPLAY_INSTALL_TARGET = YES
 HIFIBERRY_MPD_CD_AUTOPLAY_SOURCE=
 
 define HIFIBERRY_MPD_CD_AUTOPLAY_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-mpd-cd-autoplay/60-apple-superdrive.rules \
+                $(TARGET_DIR)/etc/udev/rules.d/60-apple-superdrive.rules
     $(INSTALL) -D -m 0644 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-mpd-cd-autoplay/80-mpd-cd-autoplay.rules \
                 $(TARGET_DIR)/etc/udev/rules.d/80-mpd-cd-autoplay.rules
     $(INSTALL) -D -m 0755 $(BR2_EXTERNAL_HIFIBERRY_PATH)/package/hifiberry-mpd-cd-autoplay/mpd-cd-autoplay.sh \
