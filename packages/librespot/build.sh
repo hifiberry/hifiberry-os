167113b703e41a9bec64afa5525acab3455e6b02
growers12 <160653060+growers12@users.noreply.github.com>
2025-06-13 10:41:07 +0200

    bump VERSION_POSTFIX to 3 and add permissions for /var/run/librespot and event FIFO

--- packages/librespot/build.sh
+++ packages/librespot/build.sh
@@ -4,7 +4,7 @@ set -e
 
 # Configuration
 VERSION="0.6.0"
-VERSION_POSTFIX=2 # 1,2,3,... unset when VERSION increases to start from 0 again
+VERSION_POSTFIX=3 # 1,2,3,... unset when VERSION increases to start from 0 again
 COMMIT_ID="59381ccad38ed39037392f3d2d30bf0d9593ff56"  # Optional specific commit ID to use (overrides VERSION if set)
 PACKAGE_NAME="hifiberry-librespot"
 DOCKER_TAG="librespot-build-env"
