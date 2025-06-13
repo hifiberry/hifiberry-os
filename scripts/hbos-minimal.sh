945702b7069a7c61b091a1bb835fa0a8dd73f94d
HiFiBerry <info@hifiberry.com>
2025-06-13 13:46:26 +0200

    Add hifiberry-eeprom to installation script for HiFiBerryOS

--- scripts/hbos-minimal.sh
+++ scripts/hbos-minimal.sh
@@ -1,4 +1,4 @@
 sudo apt update
 sudo apt install -y sox i2c-tools
-sudo apt install -y python3-configurator  
+sudo apt install -y python3-configurator hifiberry-eeprom 
 sudo config-configtxt --default-config
