5f75363760225eb16867eb0b3152026fabdc16a8
growers12 <160653060+growers12@users.noreply.github.com>
2025-06-13 13:43:58 +0200

    Added scripts to install HiFiBerryOS (not yet ready)

---
+++ scripts/hbos-minimal.sh
@@ -0,0 +1,4 @@
+sudo apt update
+sudo apt install -y sox i2c-tools
+sudo apt install -y python3-configurator  
+sudo config-configtxt --default-config
---
+++ scripts/hbos-player.sh
@@ -0,0 +1,3 @@
+sudo apt update
+sudo apt install -y hifiberry-pipewire
+sudo apt install -y  hifiberry-raat  hifiberry-shairport hifiberry-squeezelite hifiberry-mpd hifiberry-librespot
