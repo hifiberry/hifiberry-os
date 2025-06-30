6bec53112dc70b51691ad042465ede35d0118201
HiFiBerry <info@hifiberry.com>
2025-06-30 15:43:11 +0200

    Refactor cleanup scripts to ensure safe removal of build artifacts

--- packages/mpd/clean.sh
+++ packages/mpd/clean.sh
@@ -4,27 +4,6 @@ set -e
 
 echo "Cleaning MPD build artifacts..."
 
-# Clean the source directory
-if [ -d "src" ]; then
-    cd src
-    
-    # Clean debian build artifacts
-    rm -rf debian/.debhelper/
-    rm -rf debian/hifiberry-mpd/
-    rm -rf debian/files
-    rm -rf debian/*.substvars
-    rm -rf debian/*.log
-    rm -rf debian/tmp/
-    
-    # Clean downloaded sources
-    rm -rf mpd-source/
-    rm -rf mpc-source/
-    rm -rf mpc-install/
-    rm -f *.tar.gz
-    
-    cd ..
-fi
-
 # Clean output directory
 rm -rf out/
 
