aae2c9416e5e4209582ff42f3b2c667a2507cff7
HiFiBerry <info@hifiberry.com>
2025-08-05 16:04:41 +0100

    refactor: Update test script to use dynamic path and improve output formatting

--- packages/testtools/src/testscripts/c1/test.sh
+++ packages/testtools/src/testscripts/c1/test.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
+MYDIR=`dirname $0`
+PATH=$PATH:$MYDIR
+cd /tmp
+
 # Configure the hardware
 sudo modprobe i2c-dev
-./configure-hardware
+$MYDIR/configure-hardware
 
 # Install DSP profile
-banner "Prog. DSP"
-dsptoolkit install-profile ./dsp1.xml
-banner "Done"
+figlet "Programming DSP"
+dsptoolkit install-profile $MYDIR/dsp1.xml
+figlet "Done"
 
 
 OVERALL_STATUS=0
 
 # --- Test 1: Analog ---
-banner "Testing"
+figlet "Testing"
 
 echo "Running analog test..."
-python ./routing.py --matrix in_analog --execute
+python $MYDIR/routing.py --matrix in_analog --execute
 
 play -n synth 3 sine 1000 &
 PLAY_PID=$!
 
-./check-rms 2 1 0.5 1
+$MYDIR/check-rms 2 1 0.5 1
 RMS_ANALOG=$?
 
 wait "$PLAY_PID"
@@ -32,12 +36,12 @@ fi
 
 # --- Test 2: AES ---
 echo "Running AES test..."
-python ./routing.py --matrix in_aes --execute
+python $MYDIR/routing.py --matrix in_aes --execute
 
 play -n synth 3 sine 1000 &
 PLAY_PID=$!
 
-./check-rms 2 1 0.5 1
+$MYDIR/check-rms 2 1 0.5 1
 RMS_AES=$?
 
 wait "$PLAY_PID"
@@ -49,15 +53,15 @@ fi
 # --- Show banners ---
 echo
 if [ "$RMS_ANALOG" -eq 0 ]; then
-    banner "Analog OK"
+    figlet "Analog OK"
 else
-    banner "Fail analog"
+    figlet "Fail analog"
 fi
 
 if [ "$RMS_AES" -eq 0 ]; then
-    banner "AES OK"
+    figlet "AES OK"
 else
-    banner "Fail AES"
+    figlet "Fail AES"
 fi
 
 # Exit status
