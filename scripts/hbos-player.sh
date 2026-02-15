9fcec8c9094657a38a5c75d0019dfee80f065e75
Lars Grootkarzijn <larsgrootkarzijnn@gmail.com>
2026-02-15 21:29:06 +0100

    Update to use unshare mode

--- scripts/hbos-player.sh
+++ scripts/hbos-player.sh
@@ -1,3 +1,3 @@
 sudo apt update
 sudo apt install -y hifiberry-pipewire
-sudo apt install -y  hifiberry-raat  hifiberry-shairport hifiberry-squeezelite hifiberry-mpd hifiberry-librespot
+sudo apt install -y hifiberry-raat hifiberry-shairport hifiberry-squeezelite hifiberry-mpd hifiberry-librespot
