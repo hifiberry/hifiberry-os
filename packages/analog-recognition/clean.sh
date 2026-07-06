cae8666a2656b05112e46f902885c3c75c102df1
HiFiBerry <support@hifiberry.com>
2026-07-06 13:07:55 +0200

    fix(analog-recognition): cd to script dir in clean.sh; also prune dbgsym artifacts

--- packages/analog-recognition/clean.sh
+++ packages/analog-recognition/clean.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+cd "$(dirname "$0")"
 echo "Cleaning up analog-recognition build artifacts..."
 rm -rf analog-recognition
 rm -f hifiberry-analog-recognition_*
+rm -f hifiberry-analog-recognition-*
 echo "Cleaned up analog-recognition build artifacts."
